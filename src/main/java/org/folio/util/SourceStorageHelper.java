65db430361fd562ba201e23a87e7cb301f17af77
Kateryna Senchenko <kateryna_senchenko@epam.com>
2019-01-09 14:47:04 +0200

    Fixed security vulnerabilities

--- src/main/java/org/folio/util/SourceStorageHelper.java
+++ src/main/java/org/folio/util/SourceStorageHelper.java
@@ -3,7 +3,7 @@ package org.folio.util;
 import io.vertx.core.Future;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.apache.http.HttpStatus;
+import org.folio.HttpStatus;
 import org.folio.rest.tools.utils.ValidationHelper;
 
 import javax.ws.rs.BadRequestException;
@@ -20,13 +20,13 @@ public class SourceStorageHelper {
 
   public static Response mapExceptionToResponse(Throwable throwable) {
     if (throwable instanceof BadRequestException) {
-      return Response.status(HttpStatus.SC_BAD_REQUEST)
+      return Response.status(HttpStatus.HTTP_BAD_REQUEST.toInt())
         .type(MediaType.TEXT_PLAIN)
         .entity(throwable.getMessage())
         .build();
     }
     if (throwable instanceof NotFoundException) {
-      return Response.status(HttpStatus.SC_NOT_FOUND)
+      return Response.status(HttpStatus.HTTP_NOT_FOUND.toInt())
         .type(MediaType.TEXT_PLAIN)
         .entity(throwable.getMessage())
         .build();
@@ -35,13 +35,13 @@ public class SourceStorageHelper {
     ValidationHelper.handleError(throwable, validationFuture.completer());
     if (validationFuture.isComplete()) {
       Response response = validationFuture.result();
-      if (response.getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
+      if (response.getStatus() == HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt()) {
         LOG.error(throwable.getMessage(), throwable);
       }
       return response;
     }
     LOG.error(throwable.getMessage(), throwable);
-    return Response.status(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+    return Response.status(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt())
       .type(MediaType.TEXT_PLAIN)
       .entity(Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase())
       .build();

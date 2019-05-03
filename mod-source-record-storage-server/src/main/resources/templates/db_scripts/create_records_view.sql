53e32048ad6eec46ebb69dde013069714bd7fc09
Kateryna Senchenko <kateryna_senchenko@epam.com>
2019-05-03 15:12:06 +0300

    Revert "MODSOURCE-48 Spike. Refactoring source_record_view with foreign keys for _id fields"
    This reverts commit 2ca2c70

--- mod-source-record-storage-server/src/main/resources/templates/db_scripts/create_records_view.sql
+++ mod-source-record-storage-server/src/main/resources/templates/db_scripts/create_records_view.sql
@@ -2,24 +2,24 @@
 -- To change the view, update this script and copy it to the appropriate scripts.snippet field of the schema.json
 
 CREATE OR REPLACE VIEW records_view AS
-SELECT records._id,
-       json_build_object('id', records.jsonb ->> 'id',
-                         'snapshotId', records.jsonb ->> 'snapshotId',
-                         'matchedProfileId', records.jsonb ->> 'matchedProfileId',
-                         'matchedId', records.jsonb ->> 'matchedId',
-                         'generation', records.jsonb ->> 'generation',
-                         'recordType', records.jsonb ->> 'recordType',
-                         'deleted', records.jsonb ->> 'deleted',
-                         'additionalInfo', records.jsonb -> 'additionalInfo',
-                         'metadata', records.jsonb -> 'metadata',
-                         'rawRecord', raw_records.jsonb,
-                         'parsedRecord', COALESCE(marc_records.jsonb),
-                         'errorRecord', error_records.jsonb)
-         AS jsonb
-FROM records
-       JOIN raw_records ON records.jsonb ->> 'rawRecordId' = raw_records.jsonb ->> 'id'
-       LEFT JOIN marc_records ON records.jsonb ->> 'parsedRecordId' = marc_records.jsonb ->> 'id'
-       LEFT JOIN error_records ON records.jsonb ->> 'errorRecordId' = error_records.jsonb ->> 'id';
+  SELECT records._id,
+  json_build_object('id', records.jsonb->>'id',
+          'snapshotId', records.jsonb->>'snapshotId',
+					'matchedProfileId', records.jsonb->>'matchedProfileId',
+					'matchedId', records.jsonb->>'matchedId',
+					'generation', records.jsonb->>'generation',
+					'recordType', records.jsonb->>'recordType',
+					'deleted', records.jsonb->>'deleted',
+					'additionalInfo', records.jsonb->'additionalInfo',
+					'metadata', records.jsonb->'metadata',
+ 					'rawRecord', raw_records.jsonb,
+ 					'parsedRecord', COALESCE(marc_records.jsonb),
+					'errorRecord', error_records.jsonb)
+					AS jsonb
+   FROM records
+     JOIN raw_records ON records.jsonb->>'rawRecordId' = raw_records.jsonb->>'id'
+     LEFT JOIN marc_records ON records.jsonb->>'parsedRecordId' = marc_records.jsonb->>'id'
+     LEFT JOIN error_records ON records.jsonb->>'errorRecordId' = error_records.jsonb->>'id';
 
 -- to add a table that stores another type of parsed record,
 -- add LEFT JOIN on that table and its jsonb field as a param of COALESCE, for example:
@@ -45,17 +45,24 @@ FROM records
 --
 --Similarly to update source_records_view the following script has to be updated and copied to the appropriate scripts.snippet field of the schema.json
 CREATE OR REPLACE VIEW source_records_view AS
-SELECT records._id,
-       json_build_object('recordId', records.jsonb ->> 'id',
-                         'snapshotId', records.jsonb ->> 'snapshotId',
-                         'recordType', records.jsonb ->> 'recordType',
-                         'deleted', records.jsonb ->> 'deleted',
-                         'additionalInfo', records.jsonb -> 'additionalInfo',
-                         'metadata', records.jsonb -> 'metadata',
-                         'rawRecord', raw_records.jsonb,
-                         'parsedRecord', COALESCE(marc_records.jsonb))
-         AS jsonb
-FROM records
-       JOIN raw_records ON records.rawrecordid = raw_records._id
-       LEFT JOIN marc_records ON records.parsedrecordid = marc_records._id
-WHERE records.parsedrecordid IS NOT NULL;
+  SELECT records._id,
+  json_build_object('recordId', records.jsonb->>'id',
+          'snapshotId', records.jsonb->>'snapshotId',
+					'recordType', records.jsonb->>'recordType',
+					'deleted', records.jsonb->>'deleted',
+					'additionalInfo', records.jsonb->'additionalInfo',
+					'metadata', records.jsonb->'metadata',
+					'rawRecord', raw_records.jsonb,
+ 					'parsedRecord', COALESCE(marc_records.jsonb))
+					AS jsonb
+   FROM records
+     JOIN raw_records ON records.jsonb->>'rawRecordId' = raw_records.jsonb->>'id'
+     LEFT JOIN marc_records ON records.jsonb->>'parsedRecordId' = marc_records.jsonb->>'id'
+     WHERE records.jsonb->>'parsedRecordId' IS NOT NULL;
+
+
+CREATE INDEX records_raw_record_id_idx_btree ON records USING BTREE ((jsonb ->> 'rawRecordId'));
+CREATE INDEX records_parsed_record_id_idx_btree ON records USING BTREE ((jsonb ->> 'parsedRecordId'));
+CREATE INDEX raw_records_id_idx_btree ON raw_records USING BTREE ((jsonb ->> 'id'));
+CREATE INDEX marc_records_id_idx_btree ON marc_records USING BTREE ((jsonb ->> 'id'));
+CREATE INDEX error_records_id_idx_btree ON error_records USING BTREE ((jsonb ->> 'id'));

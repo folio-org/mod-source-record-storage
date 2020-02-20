089ef061126ff6effc98497f31569b045417d3f8
Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>
2020-02-20 16:44:37 +0200

    MODSOURCE-96 - Create event handler to update instance id to MARC bibliographic record (#115)

--- mod-source-record-storage-server/src/main/java/org/folio/rest/impl/SourceStorageHandlersImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/rest/impl/SourceStorageHandlersImpl.java
@@ -7,8 +7,11 @@ import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.folio.dataimport.util.ExceptionHelper;
 import org.folio.rest.jaxrs.resource.SourceStorageHandlers;
+import org.folio.rest.tools.utils.TenantTool;
+import org.folio.services.EventHandlingService;
+import org.folio.spring.SpringContextUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.core.Response;
 import java.util.Map;
@@ -17,21 +20,24 @@ public class SourceStorageHandlersImpl implements SourceStorageHandlers {
 
   private static final Logger LOG = LoggerFactory.getLogger(SourceStorageHandlersImpl.class);
 
+  @Autowired
+  private EventHandlingService instanceCreatedEventHandleService;
+
+  private String tenantId;
+
   public SourceStorageHandlersImpl(Vertx vertx, String tenantId) { //NOSONAR
+    SpringContextUtil.autowireDependencies(this, Vertx.currentContext());
+    this.tenantId = TenantTool.calculateTenantId(tenantId);
   }
 
   @Override
   public void postSourceStorageHandlersCreatedInventoryInstance(String entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     vertxContext.runOnContext(v -> {
-      try {
-        // endpoint will be implemented in https://issues.folio.org/browse/MODSOURCE-96
-        LOG.info("Event was received: {}", entity);
-        Future.succeededFuture((Response) SourceStorageHandlers.PostSourceStorageHandlersCreatedInventoryInstanceResponse.respond200())
-          .setHandler(asyncResultHandler);
-      } catch (Exception e) {
-        LOG.error("Failed to handle event", e);
-        asyncResultHandler.handle(Future.succeededFuture(ExceptionHelper.mapExceptionToResponse(e)));
-      }
+      LOG.debug("Event CREATED_INVENTORY_INSTANCE was received: {}", entity);
+      asyncResultHandler.handle(Future.succeededFuture(PostSourceStorageHandlersCreatedInventoryInstanceResponse.respond200()));
+
+      // response status doesn't depend on event handling result
+      instanceCreatedEventHandleService.handle(entity, tenantId);
     });
   }
 }
---
+++ mod-source-record-storage-server/src/main/java/org/folio/services/util/AdditionalFieldsUtil.java
@@ -0,0 +1,93 @@
+package org.folio.services.util;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.apache.commons.collections4.CollectionUtils;
+import org.folio.rest.jaxrs.model.Record;
+import org.marc4j.MarcJsonReader;
+import org.marc4j.MarcJsonWriter;
+import org.marc4j.MarcReader;
+import org.marc4j.MarcStreamWriter;
+import org.marc4j.MarcWriter;
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.MarcFactory;
+import org.marc4j.marc.VariableField;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * Util to work with additional fields
+ */
+public class AdditionalFieldsUtil {
+
+  public static final String TAG_999 = "999";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AdditionalFieldsUtil.class);
+  private static final char INDICATOR = 'f';
+
+  private AdditionalFieldsUtil() {
+  }
+
+  /**
+   * Adds field if it does not exist and a subfield with a value to that field
+   *
+   * @param record   record that needs to be updated
+   * @param field    field that should contain new subfield
+   * @param subfield new subfield to add
+   * @param value    value of the subfield to add
+   * @return true if succeeded, false otherwise
+   */
+  public static boolean addFieldToMarcRecord(Record record, String field, char subfield, String value) {
+    boolean result = false;
+    try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+      if (record != null && record.getParsedRecord() != null && record.getParsedRecord().getContent() != null) {
+        MarcReader reader = buildMarcReader(record);
+        MarcWriter streamWriter = new MarcStreamWriter(new ByteArrayOutputStream());
+        MarcJsonWriter jsonWriter = new MarcJsonWriter(os);
+        MarcFactory factory = MarcFactory.newInstance();
+        if (reader.hasNext()) {
+          org.marc4j.marc.Record marcRecord = reader.next();
+          VariableField variableField = getSingleFieldByIndicators(marcRecord.getVariableFields(field));
+          DataField dataField;
+          if (variableField != null
+            && ((DataField) variableField).getIndicator1() == INDICATOR
+            && ((DataField) variableField).getIndicator2() == INDICATOR)
+          {
+            dataField = (DataField) variableField;
+            marcRecord.removeVariableField(variableField);
+          } else {
+            dataField = factory.newDataField(field, INDICATOR, INDICATOR);
+          }
+          dataField.addSubfield(factory.newSubfield(subfield, value));
+          marcRecord.addVariableField(dataField);
+          // use stream writer to recalculate leader
+          streamWriter.write(marcRecord);
+          jsonWriter.write(marcRecord);
+          record.setParsedRecord(record.getParsedRecord().withContent(new JsonObject(new String(os.toByteArray())).encode()));
+          result = true;
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Failed to add additional subfield {} for field {} to record {}", e, subfield, field, record.getId());
+    }
+    return result;
+  }
+
+  private static MarcReader buildMarcReader(Record record) {
+    return new MarcJsonReader(new ByteArrayInputStream(record.getParsedRecord().getContent().toString().getBytes(StandardCharsets.UTF_8)));
+  }
+
+  private static VariableField getSingleFieldByIndicators(List<VariableField> list) {
+    if (CollectionUtils.isEmpty(list)) {
+      return null;
+    }
+    return list.stream()
+      .filter(f -> ((DataField) f).getIndicator1() == INDICATOR && ((DataField) f).getIndicator2() == INDICATOR)
+      .findFirst()
+      .orElse(null);
+  }
+}

9c3345d237db786ff32bc28a65dc5902c1f134fe
Oleksii Kuzminov <40833111+OleksiiKuzminov@users.noreply.github.com>
2020-04-03 00:21:09 +0300

    MODDICORE-38 DataImportEventPayload is not processing correctly (#127)
    * MODDICORE-38 DataImportEventPayload is not processing correctly

---
+++ mod-source-record-storage-server/src/test/java/org/folio/services/AdditionalFieldsUtilTest.java
@@ -0,0 +1,188 @@
+package org.folio.services;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.apache.commons.lang3.StringUtils;
+import org.folio.rest.impl.TestUtil;
+import org.folio.rest.jaxrs.model.ParsedRecord;
+import org.folio.rest.jaxrs.model.Record;
+import org.folio.services.util.AdditionalFieldsUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class AdditionalFieldsUtilTest {
+
+  private static final String PARSED_RECORD_PATH = "src/test/resources/parsedRecord.json";
+
+  @Test
+  public void shouldAddInstanceIdSubfield() throws IOException {
+    // given
+    String recordId = UUID.randomUUID().toString();
+    String instanceId = UUID.randomUUID().toString();
+
+    String parsedRecordContent = TestUtil.readFileFromPath(PARSED_RECORD_PATH);
+    ParsedRecord parsedRecord = new ParsedRecord();
+    String leader = new JsonObject(parsedRecordContent).getString("leader");
+    parsedRecord.setContent(parsedRecordContent);
+    Record record = new Record().withId(recordId).withParsedRecord(parsedRecord);
+    // when
+    boolean addedSourceRecordId = AdditionalFieldsUtil.addFieldToMarcRecord(record, AdditionalFieldsUtil.TAG_999, 's', recordId);
+    boolean addedInstanceId = AdditionalFieldsUtil.addFieldToMarcRecord(record, AdditionalFieldsUtil.TAG_999, 'i', instanceId);
+    // then
+    Assert.assertTrue(addedSourceRecordId);
+    Assert.assertTrue(addedInstanceId);
+    JsonObject content = new JsonObject(parsedRecord.getContent().toString());
+    JsonArray fields = content.getJsonArray("fields");
+    String newLeader = content.getString("leader");
+    Assert.assertNotEquals(leader, newLeader);
+    Assert.assertTrue(!fields.isEmpty());
+    int totalFieldsCount = 0;
+    for (int i = fields.size(); i-- > 0; ) {
+      JsonObject targetField = fields.getJsonObject(i);
+      if (targetField.containsKey(AdditionalFieldsUtil.TAG_999)) {
+        JsonArray subfields = targetField.getJsonObject(AdditionalFieldsUtil.TAG_999).getJsonArray("subfields");
+        for (int j = subfields.size(); j-- > 0; ) {
+          JsonObject targetSubfield = subfields.getJsonObject(j);
+          if (targetSubfield.containsKey("i")) {
+            String actualInstanceId = (String) targetSubfield.getValue("i");
+            Assert.assertEquals(instanceId, actualInstanceId);
+          }
+          if (targetSubfield.containsKey("s")) {
+            String actualSourceRecordId = (String) targetSubfield.getValue("s");
+            Assert.assertEquals(recordId, actualSourceRecordId);
+          }
+        }
+        totalFieldsCount++;
+      }
+    }
+    Assert.assertEquals(2, totalFieldsCount);
+  }
+
+  @Test
+  public void shouldNotAddInstanceIdSubfieldIfNoParsedRecordContent() {
+    // given
+    Record record = new Record();
+    String instanceId = UUID.randomUUID().toString();
+    // when
+    boolean added = AdditionalFieldsUtil.addFieldToMarcRecord(record, AdditionalFieldsUtil.TAG_999, 'i', instanceId);
+    // then
+    Assert.assertFalse(added);
+    Assert.assertNull(record.getParsedRecord());
+  }
+
+  @Test
+  public void shouldNotAddInstanceIdSubfieldIfNoFieldsInParsedRecordContent() {
+    // given
+    Record record = new Record();
+    String content = StringUtils.EMPTY;
+    record.setParsedRecord(new ParsedRecord().withContent(content));
+    String instanceId = UUID.randomUUID().toString();
+    // when
+    boolean added = AdditionalFieldsUtil.addFieldToMarcRecord(record, AdditionalFieldsUtil.TAG_999, 'i', instanceId);
+    // then
+    Assert.assertFalse(added);
+    Assert.assertNotNull(record.getParsedRecord());
+    Assert.assertNotNull(record.getParsedRecord().getContent());
+    Assert.assertEquals(content, record.getParsedRecord().getContent());
+  }
+
+  @Test
+  public void shouldNotAddInstanceIdSubfieldIfCanNotConvertParsedContentToJsonObject() {
+    // given
+    Record record = new Record();
+    String content = "{fields}";
+    record.setParsedRecord(new ParsedRecord().withContent(content));
+    String instanceId = UUID.randomUUID().toString();
+    // when
+    boolean added = AdditionalFieldsUtil.addFieldToMarcRecord(record, AdditionalFieldsUtil.TAG_999, 'i', instanceId);
+    // then
+    Assert.assertFalse(added);
+    Assert.assertNotNull(record.getParsedRecord());
+    Assert.assertNotNull(record.getParsedRecord().getContent());
+    Assert.assertEquals(content, record.getParsedRecord().getContent());
+  }
+
+  @Test
+  public void shouldNotAddInstanceIdSubfieldIfContentHasNoFields() {
+    // given
+    Record record = new Record();
+    String content = "{\"leader\":\"01240cas a2200397\"}";
+    record.setParsedRecord(new ParsedRecord().withContent(content));
+    String instanceId = UUID.randomUUID().toString();
+    // when
+    boolean added = AdditionalFieldsUtil.addFieldToMarcRecord(record, AdditionalFieldsUtil.TAG_999, 'i', instanceId);
+    // then
+    Assert.assertFalse(added);
+    Assert.assertNotNull(record.getParsedRecord());
+    Assert.assertNotNull(record.getParsedRecord().getContent());
+  }
+
+  @Test
+  public void shouldNotAddInstanceIdSubfieldIfContentIsNull() {
+    // given
+    Record record = new Record();
+    record.setParsedRecord(new ParsedRecord().withContent(null));
+    String instanceId = UUID.randomUUID().toString();
+    // when
+    boolean added = AdditionalFieldsUtil.addFieldToMarcRecord(record, AdditionalFieldsUtil.TAG_999, 'i', instanceId);
+    // then
+    Assert.assertFalse(added);
+    Assert.assertNotNull(record.getParsedRecord());
+    Assert.assertNull(record.getParsedRecord().getContent());
+  }
+
+  @Test
+  public void shouldRemoveField() throws IOException {
+    String recordId = UUID.randomUUID().toString();
+    String parsedRecordContent = TestUtil.readFileFromPath(PARSED_RECORD_PATH);
+    ParsedRecord parsedRecord = new ParsedRecord();
+    String leader = new JsonObject(parsedRecordContent).getString("leader");
+    parsedRecord.setContent(parsedRecordContent);
+    Record record = new Record().withId(recordId).withParsedRecord(parsedRecord);
+    boolean deleted = AdditionalFieldsUtil.removeField(record, "001");
+    Assert.assertTrue(deleted);
+    JsonObject content = new JsonObject(parsedRecord.getContent().toString());
+    JsonArray fields = content.getJsonArray("fields");
+    String newLeader = content.getString("leader");
+    Assert.assertNotEquals(leader, newLeader);
+    Assert.assertFalse(fields.isEmpty());
+    for (int i = 0; i < fields.size(); i++) {
+      JsonObject targetField = fields.getJsonObject(i);
+      if (targetField.containsKey("001")) {
+        Assert.fail();
+      }
+    }
+  }
+
+  @Test
+  public void shouldAddControlledFieldToMarcRecord() throws IOException {
+    String recordId = UUID.randomUUID().toString();
+    String parsedRecordContent = TestUtil.readFileFromPath(PARSED_RECORD_PATH);
+    ParsedRecord parsedRecord = new ParsedRecord();
+    String leader = new JsonObject(parsedRecordContent).getString("leader");
+    parsedRecord.setContent(parsedRecordContent);
+    Record record = new Record().withId(recordId).withParsedRecord(parsedRecord);
+    boolean added = AdditionalFieldsUtil.addControlledFieldToMarcRecord(record, "002", "test");
+    Assert.assertTrue(added);
+    JsonObject content = new JsonObject(parsedRecord.getContent().toString());
+    JsonArray fields = content.getJsonArray("fields");
+    String newLeader = content.getString("leader");
+    Assert.assertNotEquals(leader, newLeader);
+    Assert.assertFalse(fields.isEmpty());
+    boolean passed = false;
+    for (int i = 0; i < fields.size(); i++) {
+      JsonObject targetField = fields.getJsonObject(i);
+      if (targetField.containsKey("002") && targetField.getString("002").equals("test")) {
+        passed = true;
+        break;
+      }
+    }
+    Assert.assertTrue(passed);
+  }
+}

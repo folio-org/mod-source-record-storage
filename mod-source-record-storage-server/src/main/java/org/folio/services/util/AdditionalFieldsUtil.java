e8a0cca0a8aafd0263a16f7b1ada7f46deaff4a3
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2020-04-02 23:16:31 +0300

    MODDICORE-38 DataImportEventPayload is not processing correctly

--- mod-source-record-storage-server/src/main/java/org/folio/services/util/AdditionalFieldsUtil.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/util/AdditionalFieldsUtil.java
@@ -3,13 +3,15 @@ package org.folio.services.util;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.folio.rest.jaxrs.model.Record;
 import org.marc4j.MarcJsonReader;
 import org.marc4j.MarcJsonWriter;
 import org.marc4j.MarcReader;
 import org.marc4j.MarcStreamWriter;
 import org.marc4j.MarcWriter;
+import org.marc4j.marc.ControlField;
 import org.marc4j.marc.DataField;
 import org.marc4j.marc.MarcFactory;
 import org.marc4j.marc.VariableField;
@@ -18,13 +20,18 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Util to work with additional fields
  */
-public class AdditionalFieldsUtil {
+public final class AdditionalFieldsUtil {
 
   public static final String TAG_999 = "999";
+
+  private static final String HR_ID_FROM_FIELD = "001";
+  private static final String HR_ID_TO_FIELD = "035";
+  private static final String HR_ID_FIELD = "hrid";
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AdditionalFieldsUtil.class);
   private static final char INDICATOR = 'f';
@@ -51,12 +58,12 @@ public class AdditionalFieldsUtil {
         MarcFactory factory = MarcFactory.newInstance();
         if (reader.hasNext()) {
           org.marc4j.marc.Record marcRecord = reader.next();
-          VariableField variableField = getSingleFieldByIndicators(marcRecord.getVariableFields(field));
+          VariableField variableField = getSingleFieldByIndicators(marcRecord.getVariableFields(field), INDICATOR, INDICATOR);
           DataField dataField;
           if (variableField != null
             && ((DataField) variableField).getIndicator1() == INDICATOR
-            && ((DataField) variableField).getIndicator2() == INDICATOR)
-          {
+            && ((DataField) variableField).getIndicator2() == INDICATOR
+          ) {
             dataField = (DataField) variableField;
             marcRecord.removeVariableField(variableField);
           } else {
@@ -77,16 +84,124 @@ public class AdditionalFieldsUtil {
     return result;
   }
 
+  /**
+   * Adds new controlled field to marc record
+   *
+   * @param record record that needs to be updated
+   * @param field  tag of controlled field
+   * @param value  value of the field to add
+   * @return true if succeeded, false otherwise
+   */
+  public static boolean addControlledFieldToMarcRecord(Record record, String field, String value) {
+    boolean result = false;
+    try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+      if (record != null && record.getParsedRecord() != null && record.getParsedRecord().getContent() != null) {
+        MarcReader reader = buildMarcReader(record);
+        MarcWriter streamWriter = new MarcStreamWriter(new ByteArrayOutputStream());
+        MarcJsonWriter jsonWriter = new MarcJsonWriter(os);
+        MarcFactory factory = MarcFactory.newInstance();
+        if (reader.hasNext()) {
+          org.marc4j.marc.Record marcRecord = reader.next();
+          ControlField dataField = factory.newControlField(field, value);
+          marcRecord.addVariableField(dataField);
+          // use stream writer to recalculate leader
+          streamWriter.write(marcRecord);
+          jsonWriter.write(marcRecord);
+          record.setParsedRecord(record.getParsedRecord().withContent(new JsonObject(new String(os.toByteArray())).encode()));
+          result = true;
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Failed to add additional controlled field {) to record {}", e, field, record.getId());
+    }
+    return result;
+  }
+
+  /**
+   * remove field from marc record
+   *
+   * @param record record that needs to be updated
+   * @param field  tag of the field
+   * @return true if succeeded, false otherwise
+   */
+  public static boolean removeField(Record record, String field) {
+    boolean result = false;
+    try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+      if (record != null && record.getParsedRecord() != null && record.getParsedRecord().getContent() != null) {
+        MarcReader reader = buildMarcReader(record);
+        MarcWriter marcStreamWriter = new MarcStreamWriter(new ByteArrayOutputStream());
+        MarcJsonWriter marcJsonWriter = new MarcJsonWriter(baos);
+        if (reader.hasNext()) {
+          org.marc4j.marc.Record marcRecord = reader.next();
+          VariableField variableField = marcRecord.getVariableField(field);
+          if (variableField != null) {
+            marcRecord.removeVariableField(variableField);
+          }
+          // use stream writer to recalculate leader
+          marcStreamWriter.write(marcRecord);
+          marcJsonWriter.write(marcRecord);
+          record.setParsedRecord(record.getParsedRecord().withContent(new JsonObject(new String(baos.toByteArray())).encode()));
+          result = true;
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Failed to remove controlled field {) from record {}", e, field, record.getId());
+    }
+    return result;
+  }
+
+  /**
+   * Read value from controlled field in marc record
+   *
+   * @param record - marc record
+   * @param tag    - tag to read
+   * @return - value from field
+   */
+  public static String getValueFromControlledField(Record record, String tag) {
+    try {
+      MarcReader reader = buildMarcReader(record);
+      if (reader.hasNext()) {
+        org.marc4j.marc.Record marcRecord = reader.next();
+        Optional<ControlField> controlField = marcRecord.getControlFields()
+          .stream()
+          .filter(field -> field.getTag().equals(tag))
+          .findFirst();
+        if (controlField.isPresent()) {
+          return controlField.get().getData();
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Failed to read controlled field {) from record {}", e, tag, record.getId());
+      return null;
+    }
+    return null;
+  }
+
+  /**
+   * Move original marc hrId to 035 tag and assign created by inventory hrId into 001 tag
+   *
+   * @param recordInstancePair - pair of related instance and record
+   */
+  public static void fillHrIdFieldInMarcRecord(Pair<Record, JsonObject> recordInstancePair) {
+    String hrId = recordInstancePair.getValue().getString(HR_ID_FIELD);
+    String originalHrId = getValueFromControlledField(recordInstancePair.getKey(), HR_ID_FROM_FIELD);
+    if (StringUtils.isNotEmpty(hrId) && StringUtils.isNotEmpty(originalHrId)) {
+      removeField(recordInstancePair.getKey(), HR_ID_FROM_FIELD);
+      addControlledFieldToMarcRecord(recordInstancePair.getKey(), HR_ID_FROM_FIELD, hrId);
+      addControlledFieldToMarcRecord(recordInstancePair.getKey(), HR_ID_TO_FIELD, originalHrId);
+    }
+  }
+
   private static MarcReader buildMarcReader(Record record) {
     return new MarcJsonReader(new ByteArrayInputStream(record.getParsedRecord().getContent().toString().getBytes(StandardCharsets.UTF_8)));
   }
 
-  private static VariableField getSingleFieldByIndicators(List<VariableField> list) {
-    if (CollectionUtils.isEmpty(list)) {
+  private static VariableField getSingleFieldByIndicators(List<VariableField> list, char ind1, char ind2) {
+    if (list == null || list.isEmpty()) {
       return null;
     }
     return list.stream()
-      .filter(f -> ((DataField) f).getIndicator1() == INDICATOR && ((DataField) f).getIndicator2() == INDICATOR)
+      .filter(f -> ((DataField) f).getIndicator1() == ind1 && ((DataField) f).getIndicator2() == ind2)
       .findFirst()
       .orElse(null);
   }

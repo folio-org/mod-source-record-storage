3bafa818fae5608cbcfd187193112f03575354fd
Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>
2020-04-07 11:27:28 +0300

    MODSOURCE-117 - Added script to delete deprecated indexes (#129)

---
+++ mod-source-record-storage-server/src/main/resources/templates/db_scripts/migration_scripts/remove_deprecated_indexes.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.marc_records_id_idx;
+DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.raw_records_id_idx;
+DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.error_records_id_idx;
+DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.records_id_idx;

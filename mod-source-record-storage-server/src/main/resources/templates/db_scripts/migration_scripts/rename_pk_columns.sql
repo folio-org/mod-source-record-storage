e20bbf5545d54673b7095a4d213e827c0014ad13
Ruslan Lavrov <ruslan_lavrov@epam.com>
2020-04-21 18:00:51 +0300

    Fixed script for pk columns

--- mod-source-record-storage-server/src/main/resources/templates/db_scripts/migration_scripts/rename_pk_columns.sql
+++ mod-source-record-storage-server/src/main/resources/templates/db_scripts/migration_scripts/rename_pk_columns.sql
@@ -1,7 +1,6 @@
 -- Scripts rename all columns with name "_id" to "id"
-DO $$ BEGIN IF EXISTS(SELECT * FROM information_schema.columns WHERE table_name='snapshots' AND column_name='_id') THEN ALTER TABLE ${myuniversity}_${mymodule}.snapshots RENAME COLUMN _id TO id; END IF; END $$;
-DO $$ BEGIN IF EXISTS(SELECT * FROM information_schema.columns WHERE table_name='error_records' AND column_name='_id') THEN ALTER TABLE ${myuniversity}_${mymodule}.error_records RENAME COLUMN _id TO id; END IF; END $$;
-DO $$ BEGIN IF EXISTS(SELECT * FROM information_schema.columns WHERE table_name='marc_records' AND column_name='_id') THEN ALTER TABLE ${myuniversity}_${mymodule}.marc_records RENAME COLUMN _id TO id; END IF; END $$;
-DO $$ BEGIN IF EXISTS(SELECT * FROM information_schema.columns WHERE table_name='raw_records' AND column_name='_id') THEN ALTER TABLE ${myuniversity}_${mymodule}.raw_records RENAME COLUMN _id TO id; END IF; END $$;
-DO $$ BEGIN IF EXISTS(SELECT * FROM information_schema.columns WHERE table_name='records' AND column_name='_id') THEN ALTER TABLE ${myuniversity}_${mymodule}.records RENAME COLUMN _id TO id; END IF; END $$;
-
+DO $$ BEGIN ALTER TABLE snapshots RENAME COLUMN _id TO id; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE error_records RENAME COLUMN _id TO id; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE marc_records RENAME COLUMN _id TO id; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE raw_records RENAME COLUMN _id TO id; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE records RENAME COLUMN _id TO id; EXCEPTION WHEN OTHERS THEN END; $$;

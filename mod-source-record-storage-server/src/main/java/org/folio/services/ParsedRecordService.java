af3b067fa842d50ec63174f20bbb34281301883f
William Welling <wwelling@library.tamu.edu>
2020-05-18 18:31:05 -0500

    parsed record batch update

--- mod-source-record-storage-server/src/main/java/org/folio/services/ParsedRecordService.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/ParsedRecordService.java
@@ -3,10 +3,26 @@ package org.folio.services;
 import org.folio.dao.query.ParsedRecordQuery;
 import org.folio.rest.jaxrs.model.ParsedRecord;
 import org.folio.rest.jaxrs.model.ParsedRecordCollection;
+import org.folio.rest.jaxrs.model.ParsedRecordsBatchResponse;
+import org.folio.rest.jaxrs.model.RecordCollection;
+
+import io.vertx.core.Future;
 
 /**
  * {@link ParsedRecord} service
  */
 public interface ParsedRecordService extends EntityService<ParsedRecord, ParsedRecordCollection, ParsedRecordQuery> {
+
+  /**
+   * Update parsed records from collection of records and external relations ids
+   * in one transaction
+   *
+   * @param recordCollection collection of records from which parsed records will
+   *                         be updated
+   * @param tenantId         tenant id
+   * @return future with response containing list of successfully updated records
+   *         and error messages for records that were not updated
+   */
+  public Future<ParsedRecordsBatchResponse> updateParsedRecords(RecordCollection recordCollection, String tenantId);
 
 }
--- mod-source-record-storage-server/src/main/java/org/folio/services/impl/ParsedRecordServiceImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/impl/ParsedRecordServiceImpl.java
@@ -1,15 +1,54 @@
 package org.folio.services.impl;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.BadRequestException;
+
 import org.folio.dao.ParsedRecordDao;
 import org.folio.dao.query.ParsedRecordQuery;
 import org.folio.rest.jaxrs.model.ParsedRecord;
 import org.folio.rest.jaxrs.model.ParsedRecordCollection;
+import org.folio.rest.jaxrs.model.ParsedRecordsBatchResponse;
+import org.folio.rest.jaxrs.model.RecordCollection;
 import org.folio.services.AbstractEntityService;
 import org.folio.services.ParsedRecordService;
 import org.springframework.stereotype.Service;
 
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+
 @Service
-public class ParsedRecordServiceImpl extends AbstractEntityService<ParsedRecord, ParsedRecordCollection, ParsedRecordQuery, ParsedRecordDao>
-    implements ParsedRecordService {
+public class ParsedRecordServiceImpl extends AbstractEntityService<ParsedRecord, ParsedRecordCollection, ParsedRecordQuery, ParsedRecordDao> implements ParsedRecordService {
+
+  @Override
+  public Future<ParsedRecordsBatchResponse> updateParsedRecords(RecordCollection recordCollection, String tenantId) {
+    List<Future> futures = recordCollection.getRecords().stream()
+      .peek(record -> validateParsedRecordId(record.getParsedRecord()))
+      .map(record -> dao.update(record.getParsedRecord(), tenantId))
+      .collect(Collectors.toList());
+    Promise<ParsedRecordsBatchResponse> promise = Promise.promise();
+    CompositeFuture.join(futures).onComplete(ar -> {
+      ParsedRecordsBatchResponse response = new ParsedRecordsBatchResponse();
+      futures.forEach(update -> {
+        if (update.failed()) {
+          response.getErrorMessages().add(update.cause().getMessage());
+        } else {
+          response.getParsedRecords().add((ParsedRecord) update.result());
+        }
+      });
+      response.setTotalRecords(response.getParsedRecords().size());
+      promise.complete(response);
+    });
+    return promise.future();
+  }
+
+  private void validateParsedRecordId(ParsedRecord record) {
+    if (Objects.isNull(record.getId())) {
+      throw new BadRequestException("Each parsed record should contain an id");
+    }
+  }
 
 }

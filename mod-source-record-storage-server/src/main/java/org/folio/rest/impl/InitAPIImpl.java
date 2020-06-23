86c5628110ab968474cbf2ee7057a48ee101fe0b
William Welling <wwelling@library.tamu.edu>
2020-06-23 16:15:03 -0500

    remove unused snapshot deserializer

--- mod-source-record-storage-server/src/main/java/org/folio/rest/impl/InitAPIImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/rest/impl/InitAPIImpl.java
@@ -1,13 +1,8 @@
 package org.folio.rest.impl;
 
-import com.fasterxml.jackson.databind.JsonDeserializer;
-
 import org.folio.config.ApplicationConfig;
-import org.folio.rest.jaxrs.model.Snapshot;
 import org.folio.rest.resource.interfaces.InitAPI;
-import org.folio.rest.tools.utils.ObjectMapperTool;
 import org.folio.spring.SpringContextUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
@@ -17,16 +12,12 @@ import io.vertx.core.Vertx;
 
 public class InitAPIImpl implements InitAPI {
 
-  @Autowired
-  private JsonDeserializer<Snapshot> snapshotDeserializer;
-
   @Override
   public void init(Vertx vertx, Context context, Handler<AsyncResult<Boolean>> handler) {
     vertx.executeBlocking(
       future -> {
         SpringContextUtil.init(vertx, context, ApplicationConfig.class);
         SpringContextUtil.autowireDependencies(this, context);
-        ObjectMapperTool.registerDeserializer(Snapshot.class, snapshotDeserializer);
         future.complete();
       },
       result -> {

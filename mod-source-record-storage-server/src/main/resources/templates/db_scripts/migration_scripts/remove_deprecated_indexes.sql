51162b8fa76812e2abe480004a62a8fe5ac211ea
Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>
2020-06-24 11:53:41 +0300

    Deleted db schema definition from schema.json (#198)

--- mod-source-record-storage-server/src/main/resources/templates/db_scripts/migration_scripts/remove_deprecated_indexes.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.marc_records_id_idx;
-DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.raw_records_id_idx;
-DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.error_records_id_idx;
-DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.records_id_idx;

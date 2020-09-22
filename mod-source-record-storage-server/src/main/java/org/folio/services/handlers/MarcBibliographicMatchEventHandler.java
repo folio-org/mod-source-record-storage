f3a824cd5a92f3b279e76645b9548560af3c822b
VRohach <Volodymyr_Rohach@epam.com>
2020-09-22 14:03:28 +0300

    MODDICORE-58: 1. tests fixed[2].

--- mod-source-record-storage-server/src/main/java/org/folio/services/handlers/MarcBibliographicMatchEventHandler.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/handlers/MarcBibliographicMatchEventHandler.java
@@ -26,6 +26,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;

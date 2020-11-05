822ad192eb3fa60a46bd0f15e6679dd06a32c366
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-11-05 16:47:50 +0100

    Use MetadataUtil.populateMetadata
    Replace populateRecordsMetaData by RMB's MetadataUtil.populateMetadata.
    
    RMB 31.1.0 removed JwtUtils (RMB-707)

--- mod-source-record-storage-server/src/main/java/org/folio/rest/impl/SourceStorageBatchImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/rest/impl/SourceStorageBatchImpl.java
@@ -1,20 +1,13 @@
 package org.folio.rest.impl;
 
-import static org.folio.rest.RestVerticle.OKAPI_HEADER_TOKEN;
-import static org.folio.rest.RestVerticle.OKAPI_USERID_HEADER;
-
-import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.core.Response;
 
 import org.folio.dataimport.util.ExceptionHelper;
-import org.folio.rest.jaxrs.model.Metadata;
-import org.folio.rest.jaxrs.model.Record;
 import org.folio.rest.jaxrs.model.RecordCollection;
 import org.folio.rest.jaxrs.resource.SourceStorageBatch;
-import org.folio.rest.tools.utils.JwtUtils;
+import org.folio.rest.tools.utils.MetadataUtil;
 import org.folio.rest.tools.utils.TenantTool;
 import org.folio.services.RecordService;
 import org.folio.spring.SpringContextUtil;
@@ -25,7 +18,6 @@ import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
@@ -48,7 +40,7 @@ public class SourceStorageBatchImpl implements SourceStorageBatch {
       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     vertxContext.runOnContext(v -> {
       try {
-        populateRecordsMetaData(entity.getRecords(), okapiHeaders);
+        MetadataUtil.populateMetadata(entity.getRecords(), okapiHeaders);
         recordService.saveRecords(entity, tenantId)
           .map(recordsBatchResponse -> {
             if (!recordsBatchResponse.getRecords().isEmpty()) {
@@ -73,7 +65,7 @@ public class SourceStorageBatchImpl implements SourceStorageBatch {
       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     vertxContext.runOnContext(v -> {
       try {
-        populateRecordsMetaData(entity.getRecords(), okapiHeaders);
+        MetadataUtil.populateMetadata(entity.getRecords(), okapiHeaders);
         recordService.updateParsedRecords(entity, tenantId)
           .map(parsedRecordsBatchResponse -> {
             if (!parsedRecordsBatchResponse.getParsedRecords().isEmpty()) {
@@ -92,31 +84,4 @@ public class SourceStorageBatchImpl implements SourceStorageBatch {
       }
     });
   }
-
-  private void populateRecordsMetaData(List<Record> records, Map<String, String> okapiHeaders) {
-    String userId = okapiHeaders.get(OKAPI_USERID_HEADER);
-    String token = okapiHeaders.get(OKAPI_HEADER_TOKEN);
-    if (userId == null && token != null) {
-      userId = userIdFromToken(token);
-    }
-    Metadata md = new Metadata();
-    md.setUpdatedDate(new Date());
-    md.setUpdatedByUserId(userId);
-    md.setCreatedDate(md.getUpdatedDate());
-    md.setCreatedByUserId(userId);
-    records.forEach(instance -> instance.setMetadata(md));
-  }
-
-  private static String userIdFromToken(String token) {
-    try {
-      String[] split = token.split("\\.");
-      String json = JwtUtils.getJson(split[1]);
-      JsonObject j = new JsonObject(json);
-      return j.getString("user_id");
-    } catch (Exception e) {
-      LOG.warn("Invalid x-okapi-token: " + token, e);
-      return null;
-    }
-  }
-
 }

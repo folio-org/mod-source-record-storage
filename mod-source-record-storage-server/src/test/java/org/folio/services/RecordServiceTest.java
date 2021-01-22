f6cda497870d0df63748e490f56107cd40758f14
William Welling <wwelling@library.tamu.edu>
2021-01-21 20:10:53 -0600

    minor cleanup

--- mod-source-record-storage-server/src/test/java/org/folio/services/RecordServiceTest.java
+++ mod-source-record-storage-server/src/test/java/org/folio/services/RecordServiceTest.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import org.folio.TestMocks;

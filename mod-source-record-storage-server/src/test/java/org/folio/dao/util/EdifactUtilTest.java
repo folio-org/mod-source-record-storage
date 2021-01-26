09fcd86228b81c9d89bfd024b623c024a8b3f17a
William Welling <wwelling@library.tamu.edu>
2021-01-26 15:53:38 -0600

    add test for invalid edifact

--- mod-source-record-storage-server/src/test/java/org/folio/dao/util/EdifactUtilTest.java
+++ mod-source-record-storage-server/src/test/java/org/folio/dao/util/EdifactUtilTest.java
@@ -23,6 +23,32 @@ public class EdifactUtilTest {
 
   private static final String SOURCE_RECORD_PATH = "src/test/resources/mock/sourceRecords/e4cfe577-4015-46d8-a54d-7c9b34796955.json";
 
+  private static final String INVALID_RAW_EDIFACT_RECORD =
+    "UNA:+.? '" +
+    "UNB+UNOC:2+1694510:1943+10" +
+    "UNH+30+INVOIC:D:96A:UN:EAN008'" +
+    "BGM+380+475463'" +
+    "DTM+137:20130919:102'" +
+    "CUX+2:USD:4'" +
+    "ALC+C++++TX::28:TAX'" +
+    "MOA+8:69.09'" +
+    "LIN+1++9780199588459:EN'" +
+    "IMD+L+050+:::BLACK MARKET BRITAIN, 1939-1955.'" +
+    "IMD+L+010+:::ROODHOUSE, MARK'" +
+    "IMD+L+110+:::OXFORD'" +
+    "QTY+47:1'" +
+    "MOA+203:103.13'" +
+    "PRI+AAB:125'" +
+    "PRI+AAA:103.13'" +
+    "RFF+LI:POL-17653'" +
+    "RFF+SLI:99954761992'" +
+    "UNS+S'" +
+    "CNT+1:19'" +
+    "CNT+2:19'" +
+    "MOA+9:1818.28'" +
+    "MOA+79:1818.28'" +
+    "UNT+217+33'";
+
   private SourceRecord sourceRecord;
 
   @Before
@@ -45,6 +71,11 @@ public class EdifactUtilTest {
     String formattedEdifact = EdifactUtil.formatEdifact(rawEdifact);
     assertNotNull(formattedEdifact);
     assertEquals(sourceRecord.getParsedRecord().getFormattedContent(), formattedEdifact);
+  }
+
+  @Test(expected = EDIStreamException.class)
+  public void shouldThrowExceptionAttemptingFormatEdifact() throws IOException, EDIStreamException {
+    EdifactUtil.formatEdifact(INVALID_RAW_EDIFACT_RECORD);
   }
 
 }

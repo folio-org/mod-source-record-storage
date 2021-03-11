2fa7f0e96a0bffb29b242c7e9a924bdbfc1a5997
igor_gorchakov <Igor_Gorchakov@epam.com>
2021-03-11 18:02:51 +0200

    MODSOURCE-223 - validation for the absence or presence for incoming leaderSearchExpression and fieldsSearchExpression

--- mod-source-record-storage-server/src/main/java/org/folio/services/RecordServiceImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/RecordServiceImpl.java
@@ -131,6 +131,9 @@ public class RecordServiceImpl implements RecordService {
 
   @Override
   public Flowable<String> streamMarcRecordIds(String leaderExpression, String fieldsExpression, int offset, int limit, String tenantId) {
+    if (leaderExpression == null && fieldsExpression == null) {
+      throw new IllegalArgumentException("The 'leaderSearchExpression' and the 'fieldsSearchExpression' are missing");
+    }
     ParseLeaderResult parseLeaderResult = SearchExpressionParser.parseLeaderSearchExpression(leaderExpression);
     ParseFieldsResult parseFieldsResult = SearchExpressionParser.parseFieldsSearchExpression(fieldsExpression);
     return recordDao.streamMarcRecordIds(parseLeaderResult, parseFieldsResult, offset, limit, tenantId);

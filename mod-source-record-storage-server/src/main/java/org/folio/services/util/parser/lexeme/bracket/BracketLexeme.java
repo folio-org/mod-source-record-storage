6feedbd1e5a81db2b234b8879f8c450e7a6939af
igor_gorchakov <Igor_Gorchakov@epam.com>
2021-03-11 23:27:46 +0200

    fix code smells

--- mod-source-record-storage-server/src/main/java/org/folio/services/util/parser/lexeme/bracket/BracketLexeme.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/util/parser/lexeme/bracket/BracketLexeme.java
@@ -4,6 +4,9 @@ public class BracketLexeme {
   private static final OpenedBracketLexeme OPENED_BRACKET_LEXEME = new OpenedBracketLexeme();
   private static final ClosedBracketLexeme CLOSED_BRACKET_LEXEME = new ClosedBracketLexeme();
 
+  private BracketLexeme() {
+  }
+
   public static OpenedBracketLexeme opened() {
     return OPENED_BRACKET_LEXEME;
   }

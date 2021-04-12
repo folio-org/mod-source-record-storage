ed5d21b1b53973b9525ed4b5ddb69b4914c04951
Ruslan Lavrov <ruslan_lavrov@epam.com>
2021-04-12 16:58:47 +0300

    Add logging for event correlationId

--- mod-source-record-storage-server/src/main/java/org/folio/services/DataImportKafkaHandler.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/DataImportKafkaHandler.java
@@ -4,6 +4,7 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.kafka.client.producer.KafkaHeader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 import org.folio.DataImportEventPayload;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.folio.DataImportEventTypes.DI_ERROR;
 
@@ -25,6 +27,7 @@ import static org.folio.DataImportEventTypes.DI_ERROR;
 public class DataImportKafkaHandler implements AsyncRecordHandler<String, String> {
 
   private static final Logger LOGGER = LogManager.getLogger();
+  private static final String CORRELATION_ID_HEADER = "correlationId";
 
   private Vertx vertx;
 
@@ -36,9 +39,10 @@ public class DataImportKafkaHandler implements AsyncRecordHandler<String, String
   public Future<String> handle(KafkaConsumerRecord<String, String> record) {
     try {
       Promise<String> promise = Promise.promise();
+      String correlationId = extractCorrelationId(record.headers());
       Event event = ObjectMapperTool.getMapper().readValue(record.value(), Event.class);
       DataImportEventPayload eventPayload = new JsonObject(ZIPArchiver.unzip(event.getEventPayload())).mapTo(DataImportEventPayload.class);
-      LOGGER.debug("Data import event payload has been received with event type: {}", eventPayload.getEventType());
+      LOGGER.debug("Data import event payload has been received with event type: {} and correlationId: {}", eventPayload.getEventType(), correlationId);
 
       EventManager.handleEvent(eventPayload).whenComplete((processedPayload, throwable) -> {
         if (throwable != null) {
@@ -54,6 +58,14 @@ public class DataImportKafkaHandler implements AsyncRecordHandler<String, String
       LOGGER.error("Failed to process data import kafka record from topic {}", record.topic(), e);
       return Future.failedFuture(e);
     }
+  }
+
+  private String extractCorrelationId(List<KafkaHeader> headers) {
+    return headers.stream()
+      .filter(header -> header.key().equals(CORRELATION_ID_HEADER))
+      .findFirst()
+      .map(header -> header.value().toString())
+      .orElse(null);
   }
 
 }

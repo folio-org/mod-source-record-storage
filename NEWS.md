ef01a6f39e5c599fe5b5fd69bb2ecef5761e28b3
Ruslan Lavrov <ruslan_lavrov@epam.com>
2021-05-31 18:09:32 +0300

    MODSOURCE-301 - Cannot import GOBI EDIFACT invoice (#340)
    * Added '31B' code to exceptions as expected value
    
    * Allow code values that are not included to validation schema using EDI_VALIDATE_CONTROL_CODE_VALUES property
    
    * verify that parsed content is not null on normalization before event sending
    
    (cherry picked from commit 9553dc9fe8b62d34ea88eeec27f9776466e52a33)

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,7 @@
-## 2021-05-xx v5.0.5-SNAPSHOT
+## 2021-06-16 v5.0.5-SNAPSHOT
 * [MODSOURCE-318](https://issues.folio.org/browse/MODSOURCE-318) Set processingStartedDate for snapshot when updating via QM
 * [MODSOURCE-310](https://issues.folio.org/browse/MODSOURCE-310) Handling 001/003/035 handling in SRS for MARC bib records broken
+* [MODSOURCE-301](https://issues.folio.org/browse/MODSOURCE-301) Cannot import GOBI EDIFACT invoice
 
 ## 2021-05-22 v5.0.4
 * [MODSOURCE-280](https://issues.folio.org/browse/MODSOURCE-280) Issue with database migration for Iris release
--- mod-source-record-storage-server/src/test/java/org/folio/dao/util/EdifactUtilTest.java
+++ mod-source-record-storage-server/src/test/java/org/folio/dao/util/EdifactUtilTest.java
@@ -1,27 +1,26 @@
 package org.folio.dao.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.io.File;
-import java.io.IOException;
-
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import io.xlate.edi.stream.EDIStreamException;
 import org.folio.rest.jaxrs.model.SourceRecord;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 
-import io.xlate.edi.stream.EDIStreamException;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class EdifactUtilTest {
 
   private static final String SOURCE_RECORD_PATH = "src/test/resources/mock/sourceRecords/e4cfe577-4015-46d8-a54d-7c9b34796955.json";
+  private static final String SOURCE_RECORD_WITH_IGNORED_CODES_PATH = "src/test/resources/mock/sourceRecords/4ca9d8ac-9de5-432a-83ee-15832f09e868.json";
 
   private static final String INVALID_RAW_EDIFACT_RECORD =
     "UNA:+.? '" +
@@ -67,6 +66,15 @@ public class EdifactUtilTest {
 
   @Test
   public void shouldFormatEdifactAlreadyFormatted() throws IOException, EDIStreamException {
+    String rawEdifact = sourceRecord.getParsedRecord().getFormattedContent();
+    String formattedEdifact = EdifactUtil.formatEdifact(rawEdifact);
+    assertNotNull(formattedEdifact);
+    assertEquals(sourceRecord.getParsedRecord().getFormattedContent(), formattedEdifact);
+  }
+
+  @Test
+  public void shouldFormatEdifactWithIgnoredCodeValues() throws IOException, EDIStreamException {
+    SourceRecord sourceRecord = new ObjectMapper().readValue(new File(SOURCE_RECORD_WITH_IGNORED_CODES_PATH), SourceRecord.class);
     String rawEdifact = sourceRecord.getParsedRecord().getFormattedContent();
     String formattedEdifact = EdifactUtil.formatEdifact(rawEdifact);
     assertNotNull(formattedEdifact);

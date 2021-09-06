9d0ef3b5056986fe86db3c13bb7ed2f530f7b098
Ruslan Lavrov <ruslan_lavrov@epam.com>
2021-09-07 02:31:17 +0300

    Added jobProfileSnapshot cache

--- mod-source-record-storage-server/src/main/java/org/folio/services/DataImportKafkaHandler.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/DataImportKafkaHandler.java
@@ -4,14 +4,18 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 import io.vertx.kafka.client.producer.KafkaHeader;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
+import org.apache.logging.log4j.Logger;
 import org.folio.DataImportEventPayload;
+import org.folio.dataimport.util.OkapiConnectionParams;
+import org.folio.dataimport.util.RestUtil;
 import org.folio.dbschema.ObjectMapperTool;
 import org.folio.kafka.AsyncRecordHandler;
 import org.folio.processing.events.EventManager;
 import org.folio.processing.events.utils.ZIPArchiver;
+import org.folio.processing.exceptions.EventProcessingException;
 import org.folio.rest.jaxrs.model.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -19,7 +23,10 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
+import static java.lang.String.format;
 import static org.folio.DataImportEventTypes.DI_ERROR;
 
 @Component
@@ -28,11 +35,15 @@ public class DataImportKafkaHandler implements AsyncRecordHandler<String, String
 
   private static final Logger LOGGER = LogManager.getLogger();
   private static final String CORRELATION_ID_HEADER = "correlationId";
+  private static final String PROFILE_SNAPSHOT_ID_KEY = "profileSnapshotId";
 
   private Vertx vertx;
+  private JobProfileSnapshotCache profileSnapshotCache;
 
-  public DataImportKafkaHandler(@Autowired Vertx vertx) {
+  @Autowired
+  public DataImportKafkaHandler(Vertx vertx, JobProfileSnapshotCache profileSnapshotCache) {
     this.vertx = vertx;
+    this.profileSnapshotCache = profileSnapshotCache;
   }
 
   @Override
@@ -43,9 +54,16 @@ public class DataImportKafkaHandler implements AsyncRecordHandler<String, String
       Event event = ObjectMapperTool.getMapper().readValue(record.value(), Event.class);
       DataImportEventPayload eventPayload = new JsonObject(ZIPArchiver.unzip(event.getEventPayload())).mapTo(DataImportEventPayload.class);
       LOGGER.debug("Data import event payload has been received with event type: {} and correlationId: {}", eventPayload.getEventType(), correlationId);
-
       eventPayload.getContext().put(CORRELATION_ID_HEADER, correlationId);
-      EventManager.handleEvent(eventPayload).whenComplete((processedPayload, throwable) -> {
+
+      OkapiConnectionParams params = retrieveOkapiConnectionParams(eventPayload);
+      String jobProfileSnapshotId = eventPayload.getContext().get(PROFILE_SNAPSHOT_ID_KEY);
+      profileSnapshotCache.get(jobProfileSnapshotId, params)
+        .toCompletionStage()
+        .thenCompose(snapshotOptional -> snapshotOptional
+          .map(profileSnapshot -> EventManager.handleEvent(eventPayload))
+          .orElse(CompletableFuture.failedFuture(new EventProcessingException(format("Job profile snapshot with id '%s' does not exist", jobProfileSnapshotId)))))
+        .whenComplete((processedPayload, throwable) -> {
         if (throwable != null) {
           promise.fail(throwable);
         } else if (DI_ERROR.value().equals(processedPayload.getEventType())) {
@@ -67,6 +85,14 @@ public class DataImportKafkaHandler implements AsyncRecordHandler<String, String
       .findFirst()
       .map(header -> header.value().toString())
       .orElse(null);
+  }
+
+  private OkapiConnectionParams retrieveOkapiConnectionParams(DataImportEventPayload eventPayload) {
+    return new OkapiConnectionParams(Map.of(
+      RestUtil.OKAPI_URL_HEADER, eventPayload.getOkapiUrl(),
+      RestUtil.OKAPI_TENANT_HEADER, eventPayload.getTenant(),
+      RestUtil.OKAPI_TOKEN_HEADER, eventPayload.getToken()
+    ), this.vertx);
   }
 
 }

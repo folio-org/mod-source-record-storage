26345f8a68d2bd47920f8206821479d7672ab0eb
Pavlo Smahin <Pavlo_Smahin@epam.com>
2021-09-17 16:44:09 +0300

    MODSOURCE-376 Register holdingsPostProcessingEventHandler

--- mod-source-record-storage-server/src/main/java/org/folio/rest/impl/InitAPIImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/rest/impl/InitAPIImpl.java
@@ -15,6 +15,7 @@ import org.apache.logging.log4j.Logger;
 import org.folio.config.ApplicationConfig;
 import org.folio.processing.events.EventManager;
 import org.folio.rest.resource.interfaces.InitAPI;
+import org.folio.services.handlers.HoldingsPostProcessingEventHandler;
 import org.folio.services.handlers.InstancePostProcessingEventHandler;
 import org.folio.services.handlers.MarcAuthorityEventHandler;
 import org.folio.services.handlers.MarcBibliographicMatchEventHandler;
@@ -33,6 +34,9 @@ public class InitAPIImpl implements InitAPI {
 
   @Autowired
   private InstancePostProcessingEventHandler instancePostProcessingEventHandler;
+
+  @Autowired
+  private HoldingsPostProcessingEventHandler holdingsPostProcessingEventHandler;
 
   @Autowired
   private ModifyRecordEventHandler modifyRecordEventHandler;
@@ -73,6 +77,7 @@ public class InitAPIImpl implements InitAPI {
 
   private void registerEventHandlers() {
     EventManager.registerEventHandler(instancePostProcessingEventHandler);
+    EventManager.registerEventHandler(holdingsPostProcessingEventHandler);
     EventManager.registerEventHandler(modifyRecordEventHandler);
     EventManager.registerEventHandler(marcBibliographicMatchEventHandler);
     EventManager.registerEventHandler(marcAuthorityEventHandler);

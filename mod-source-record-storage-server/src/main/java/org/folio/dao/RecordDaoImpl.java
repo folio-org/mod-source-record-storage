8b38831bd79954441b2ff54c05868e9650d45644
Aliaksandr-Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2021-11-16 12:52:13 +0300

    MODSOURCE-405 mod-source-record-storage OOM

--- mod-source-record-storage-server/src/main/java/org/folio/dao/RecordDaoImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/dao/RecordDaoImpl.java
@@ -734,7 +734,8 @@ public class RecordDaoImpl implements RecordDao {
           .withTotalRecords(parsedRecordsUpdated.size()));
       });
     } catch (SQLException e) {
-      e.printStackTrace();
+      LOG.error("Failed to update records", e);
+      promise.fail(e);
     }
 
     return promise.future();

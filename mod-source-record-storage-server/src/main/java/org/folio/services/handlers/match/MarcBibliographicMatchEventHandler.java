c8f48c631ad8871d6b4ccb37a7b528c61c7231f5
Pavlo Smahin <Pavlo_Smahin@epam.com>
2022-01-05 14:13:03 +0200

    MODSOURCE-433 Remove unused imports

--- mod-source-record-storage-server/src/main/java/org/folio/services/handlers/match/MarcBibliographicMatchEventHandler.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/handlers/match/MarcBibliographicMatchEventHandler.java
@@ -1,19 +1,14 @@
 package org.folio.services.handlers.match;
 
-import org.folio.dao.RecordDao;
-import org.folio.rest.jaxrs.model.Record;
-import org.folio.services.util.TypeConnection;
-import org.jooq.Condition;
+import static org.folio.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_MATCHED_READY_FOR_POST_PROCESSING;
+import static org.folio.rest.jaxrs.model.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_MATCHED;
+import static org.folio.rest.jaxrs.model.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_NOT_MATCHED;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import static org.folio.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_MATCHED_READY_FOR_POST_PROCESSING;
-import static org.folio.dao.util.RecordDaoUtil.filterRecordByExternalHrid;
-import static org.folio.dao.util.RecordDaoUtil.filterRecordByExternalId;
-import static org.folio.dao.util.RecordDaoUtil.filterRecordByRecordId;
-import static org.folio.dao.util.RecordDaoUtil.filterRecordByState;
-import static org.folio.rest.jaxrs.model.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_MATCHED;
-import static org.folio.rest.jaxrs.model.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_NOT_MATCHED;
+import org.folio.dao.RecordDao;
+import org.folio.services.util.TypeConnection;
 
 /**
  * Handler for MARC-MARC matching/not-matching MARC bibliographic record by specific fields.

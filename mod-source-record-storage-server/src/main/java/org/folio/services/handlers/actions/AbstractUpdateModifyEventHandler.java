af6304f514885f0afd45d917ef3fafbb498e8f4e
Aliaksandr-Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2022-08-15 00:41:06 +0500

    MODSOURCE-528: Field 035 with hrId should not be created if MARC BIB already exists

--- mod-source-record-storage-server/src/main/java/org/folio/services/handlers/actions/AbstractUpdateModifyEventHandler.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/handlers/actions/AbstractUpdateModifyEventHandler.java
@@ -15,12 +15,19 @@ import org.folio.processing.events.services.handler.EventHandler;
 import org.folio.processing.exceptions.EventProcessingException;
 import org.folio.processing.mapping.defaultmapper.processor.parameters.MappingParameters;
 import org.folio.processing.mapping.mapper.writer.marc.MarcRecordModifier;
-import org.folio.rest.jaxrs.model.*;
+import org.folio.rest.jaxrs.model.EntityType;
+import org.folio.rest.jaxrs.model.MappingDetail;
+import org.folio.rest.jaxrs.model.Metadata;
+import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
+import org.folio.rest.jaxrs.model.Record;
+import org.folio.rest.jaxrs.model.SourceRecordCollection;
 import org.folio.services.RecordService;
 import org.folio.services.caches.MappingParametersSnapshotCache;
 import org.folio.services.util.RestUtil;
+import org.jooq.Condition;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -32,12 +39,20 @@ import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.folio.ActionProfile.Action.MODIFY;
 import static org.folio.ActionProfile.Action.UPDATE;
+import static org.folio.dao.util.RecordDaoUtil.filterRecordByExternalHrid;
+import static org.folio.dao.util.RecordDaoUtil.filterRecordByRecordId;
+import static org.folio.dao.util.RecordType.MARC_BIB;
 import static org.folio.rest.jaxrs.model.ProfileSnapshotWrapper.ContentType.ACTION_PROFILE;
+import static org.folio.rest.util.QueryParamUtil.firstNonEmpty;
+import static org.folio.services.util.AdditionalFieldsUtil.HR_ID_FIELD_SUB;
 import static org.folio.services.util.AdditionalFieldsUtil.HR_ID_FROM_FIELD;
+import static org.folio.services.util.AdditionalFieldsUtil.HR_ID_TO_FIELD;
 import static org.folio.services.util.AdditionalFieldsUtil.addControlledFieldToMarcRecord;
 import static org.folio.services.util.AdditionalFieldsUtil.fill035FieldInMarcRecordIfNotExists;
 import static org.folio.services.util.AdditionalFieldsUtil.getValueFromControlledField;
+import static org.folio.services.util.AdditionalFieldsUtil.isFieldExist;
 import static org.folio.services.util.AdditionalFieldsUtil.remove003FieldIfNeeded;
+import static org.folio.services.util.AdditionalFieldsUtil.removeField;
 
 public abstract class AbstractUpdateModifyEventHandler implements EventHandler {
 
@@ -85,19 +100,27 @@ public abstract class AbstractUpdateModifyEventHandler implements EventHandler {
         .onSuccess(v -> prepareModificationResult(payload, marcMappingOption))
         .map(v -> Json.decodeValue(payloadContext.get(modifiedEntityType().value()), Record.class))
         .onSuccess(changedRecord -> {
-          if (isHridFillingNeeded() || isUpdateOption(marcMappingOption)) {
-            addControlledFieldToMarcRecord(changedRecord, HR_ID_FROM_FIELD, hrId, true);
+          getExistingRecordsByHrIdAndMatchedId(changedRecord, hrId, payload.getTenant())
+            .onSuccess(sourceRecordCollection -> {
+              if (isHridFillingNeeded() || isUpdateOption(marcMappingOption)) {
+                addControlledFieldToMarcRecord(changedRecord, HR_ID_FROM_FIELD, hrId, true);
 
-            String changed001 = getValueFromControlledField(changedRecord, HR_ID_FROM_FIELD);
-            if (StringUtils.isNotBlank(incoming001) && !incoming001.equals(changed001)) {
-              fill035FieldInMarcRecordIfNotExists(changedRecord, incoming001);
-            }
-            remove003FieldIfNeeded(changedRecord, hrId);
-          }
-
-          increaseGeneration(changedRecord);
-          setUpdatedBy(changedRecord, userId);
-          payloadContext.put(modifiedEntityType().value(), Json.encode(changedRecord));
+                if (StringUtils.isNotBlank(incoming001) && !incoming001.equals(hrId)
+                  && (sourceRecordCollection.getTotalRecords() == 0)) {
+                  fill035FieldInMarcRecordIfNotExists(changedRecord, incoming001);
+                } else if ((sourceRecordCollection.getTotalRecords() > 0)
+                  && (isFieldExist(changedRecord, HR_ID_TO_FIELD, HR_ID_FIELD_SUB, hrId))) {
+                  removeField(changedRecord, HR_ID_TO_FIELD, HR_ID_FIELD_SUB, hrId);
+                }
+                remove003FieldIfNeeded(changedRecord, hrId);
+              }
+              increaseGeneration(changedRecord);
+              setUpdatedBy(changedRecord, userId);
+              payloadContext.put(modifiedEntityType().value(), Json.encode(changedRecord));
+            }).onFailure(throwable -> {
+              LOG.error("Error during check MARC record existence", throwable);
+              future.completeExceptionally(throwable);
+            });
         })
         .compose(changedRecord -> recordService.saveRecord(changedRecord, payload.getTenant()))
         .onSuccess(savedRecord -> {
@@ -113,6 +136,16 @@ public abstract class AbstractUpdateModifyEventHandler implements EventHandler {
       future.completeExceptionally(e);
     }
     return future;
+  }
+
+  private Future<SourceRecordCollection> getExistingRecordsByHrIdAndMatchedId(Record record, String hrId, String tenantId) {
+    try {
+      Condition condition = filterRecordByRecordId(record.getMatchedId())
+        .and(filterRecordByExternalHrid(firstNonEmpty(hrId)));
+      return recordService.getSourceRecords(condition, MARC_BIB, new ArrayList<>(), 0, 1, tenantId);
+    } catch (Exception e) {
+      return Future.failedFuture(e);
+    }
   }
 
   @Override
--- mod-source-record-storage-server/src/main/java/org/folio/services/util/AdditionalFieldsUtil.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/util/AdditionalFieldsUtil.java
@@ -45,10 +45,10 @@ public final class AdditionalFieldsUtil {
 
   public static final String HR_ID_FROM_FIELD = "001";
   private static final String HR_ID_PREFIX_FROM_FIELD = "003";
-  private static final String HR_ID_TO_FIELD = "035";
+  public static final String HR_ID_TO_FIELD = "035";
   private static final String HR_ID_FIELD = "hrid";
   private static final String ID_FIELD = "id";
-  private static final char HR_ID_FIELD_SUB = 'a';
+  public static final char HR_ID_FIELD_SUB = 'a';
   private static final char HR_ID_FIELD_IND = ' ';
   private static final String ANY_STRING = "*";
 
@@ -150,11 +150,13 @@ public final class AdditionalFieldsUtil {
   /**
    * remove field from marc record
    *
-   * @param record record that needs to be updated
-   * @param field  tag of the field
+   * @param record   record that needs to be updated
+   * @param field    tag of the field
+   * @param subfield subfield of the field
+   * @param value    value of the field
    * @return true if succeeded, false otherwise
    */
-  public static boolean removeField(Record record, String field) {
+  public static boolean removeField(Record record, String field, char subfield, String value) {
     boolean result = false;
     try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
       if (record != null && record.getParsedRecord() != null && record.getParsedRecord().getContent() != null) {
@@ -163,21 +165,47 @@ public final class AdditionalFieldsUtil {
         MarcJsonWriter marcJsonWriter = new MarcJsonWriter(baos);
         if (reader.hasNext()) {
           org.marc4j.marc.Record marcRecord = reader.next();
-          VariableField variableField = marcRecord.getVariableField(field);
-          if (variableField != null) {
-            marcRecord.removeVariableField(variableField);
+          List<VariableField> variableFields = marcRecord.getVariableFields(field);
+          for (VariableField variableField : variableFields) {
+            if (StringUtils.isEmpty(value)) {
+              marcRecord.removeVariableField(variableField);
+              result = true;
+              break;
+            } else {
+              if (variableField instanceof DataField) {
+                for (Subfield sub : ((DataField) variableField).getSubfields(subfield)) {
+                  if (isNotEmpty(sub.getData()) && sub.getData().equals(value.trim())) {
+                    marcRecord.removeVariableField(variableField);
+                    result = true;
+                    break;
+                  }
+                }
+              }
+            }
           }
-          // use stream writer to recalculate leader
-          marcStreamWriter.write(marcRecord);
-          marcJsonWriter.write(marcRecord);
-          record.setParsedRecord(record.getParsedRecord().withContent(new JsonObject(baos.toString()).encode()));
-          result = true;
+          if (result) {
+            // use stream writer to recalculate leader
+            marcStreamWriter.write(marcRecord);
+            marcJsonWriter.write(marcRecord);
+            record.setParsedRecord(record.getParsedRecord().withContent(new JsonObject(baos.toString()).encode()));
+          }
         }
       }
     } catch (Exception e) {
       LOGGER.error("Failed to remove controlled field {} from record {}", field, record.getId(), e);
     }
     return result;
+  }
+
+  /**
+   * remove field from marc record
+   *
+   * @param record record that needs to be updated
+   * @param field  tag of the field
+   * @return true if succeeded, false otherwise
+   */
+  public static boolean removeField(Record record, String field) {
+    return removeField(record, field, '\0', null);
   }
 
   /**
@@ -262,8 +290,8 @@ public final class AdditionalFieldsUtil {
    * @return true if exist
    */
   public static boolean isFieldExist(Record record, String tag, char subfield, String value) {
-    if (record != null && record.getParsedRecord() != null && record.getParsedRecord().getContent() != null) {
-      MarcReader reader = buildMarcReader(record);
+      if (record != null && record.getParsedRecord() != null && record.getParsedRecord().getContent() != null) {
+        MarcReader reader = buildMarcReader(record);
       try {
         if (reader.hasNext()) {
           org.marc4j.marc.Record marcRecord = reader.next();

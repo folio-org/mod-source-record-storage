aa726d75d231104e27696dfed55d8f350c790bf3
Roman Chernetskyi <Roman_Chernetskyi@epam.com>
2022-12-22 19:29:19 +0200

    [MODSOURCE-566] Imrove logging in DAO

--- mod-source-record-storage-server/src/main/java/org/folio/dao/SnapshotDaoImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/dao/SnapshotDaoImpl.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.dao.util.SnapshotDaoUtil;
 import org.folio.rest.jaxrs.model.Snapshot;
 import org.folio.rest.jaxrs.model.SnapshotCollection;
@@ -22,6 +24,7 @@ import io.vertx.core.Future;
 @Component
 public class SnapshotDaoImpl implements SnapshotDao {
 
+  private static final Logger LOG = LogManager.getLogger();
   private final PostgresClientFactory postgresClientFactory;
 
   @Autowired
@@ -50,16 +53,19 @@ public class SnapshotDaoImpl implements SnapshotDao {
 
   @Override
   public Future<Snapshot> saveSnapshot(Snapshot snapshot, String tenantId) {
+    LOG.trace("saveSnapshot:: Saving snapshot with jobExecutionId {} for tenant {}", snapshot.getJobExecutionId(), tenantId);
     return SnapshotDaoUtil.save(getQueryExecutor(tenantId), snapshot);
   }
 
   @Override
   public Future<Snapshot> updateSnapshot(Snapshot snapshot, String tenantId) {
+    LOG.trace("updateSnapshot:: Updating snapshot with jobExecutionId {} for tenant {}", snapshot.getJobExecutionId(), tenantId);
     return SnapshotDaoUtil.update(getQueryExecutor(tenantId), snapshot);
   }
 
   @Override
   public Future<Boolean> deleteSnapshot(String id, String tenantId) {
+    LOG.trace("deleteSnapshot:: Deleting snapshot {} for tenant {}", id, tenantId);
     return SnapshotDaoUtil.delete(getQueryExecutor(tenantId), id);
   }
 

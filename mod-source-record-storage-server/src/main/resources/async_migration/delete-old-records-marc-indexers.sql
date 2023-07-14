4208e636c8cc0818a9e257a5c3bcd64183a616ad
Ruslan Lavrov <ruslan_lavrov@epam.com>
2023-07-14 18:08:17 +0300

    Revert "MODSOURCE-636 - Implement async migration service (Nolana Service Patch) (#555)"
    This reverts commit 9c3e99b8

--- mod-source-record-storage-server/src/main/resources/async_migration/delete-old-records-marc-indexers.sql
+++
@@ -1,8 +0,0 @@
--- delete marc_indexers related to OLD records
-DELETE FROM marc_indexers
-WHERE exists(
-  SELECT 1
-  FROM records_lb
-  WHERE records_lb.id = marc_indexers.marc_id
-    AND records_lb.state = 'OLD'
-);

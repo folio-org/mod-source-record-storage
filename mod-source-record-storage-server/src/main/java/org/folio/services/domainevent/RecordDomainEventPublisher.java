39e8e3738832c194b188d04683a721efde1d85a9
pbobylev <pavel_bobylev@epam.com>
2024-07-29 16:50:37 +0500

    MODSOURCE-752: Use RAML definition for event DTO

--- mod-source-record-storage-server/src/main/java/org/folio/services/domainevent/RecordDomainEventPublisher.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/domainevent/RecordDomainEventPublisher.java
@@ -3,16 +3,23 @@ package org.folio.services.domainevent;
 import static org.folio.okapi.common.XOkapiHeaders.TENANT;
 import static org.folio.okapi.common.XOkapiHeaders.TOKEN;
 import static org.folio.okapi.common.XOkapiHeaders.URL;
+import static org.folio.rest.jaxrs.model.SourceRecordDomainEvent.EventType.SOURCE_RECORD_CREATED;
+import static org.folio.rest.jaxrs.model.SourceRecordDomainEvent.EventType.SOURCE_RECORD_UPDATED;
 import static org.folio.services.util.EventHandlingUtil.sendEventToKafka;
 
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.Json;
 import io.vertx.kafka.client.producer.KafkaHeader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.kafka.KafkaConfig;
 import org.folio.rest.jaxrs.model.Record;
+import org.folio.rest.jaxrs.model.SourceRecordDomainEvent;
+import org.folio.rest.jaxrs.model.SourceRecordDomainEvent.EventType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,19 +27,31 @@ import org.springframework.stereotype.Component;
 public class RecordDomainEventPublisher {
 
   public static final String RECORD_DOMAIN_TOPIC = "srs.source_records";
-  public static final String SOURCE_RECORD_CREATED = "SOURCE_RECORD_CREATED";
-  public static final String SOURCE_RECORD_UPDATED = "SOURCE_RECORD_UPDATED";
   private static final String RECORD_TYPE = "folio.srs.recordType";
+  private static final Logger LOG = LogManager.getLogger();
 
   @Autowired
   private KafkaConfig kafkaConfig;
 
   public void publishRecordCreated(Record created, Map<String, String> okapiHeaders) {
+    publishRecord(created, okapiHeaders, SOURCE_RECORD_CREATED);
+  }
+
+  public void publishRecordUpdated(Record updated, Map<String, String> okapiHeaders) {
+    publishRecord(updated, okapiHeaders, SOURCE_RECORD_UPDATED);
+  }
+
+  private void publishRecord(Record aRecord, Map<String, String> okapiHeaders, EventType eventType) {
     Vertx.vertx().executeBlocking(() -> {
-      var kafkaHeaders = getKafkaHeaders(okapiHeaders, created.getRecordType());
-      var key = created.getId();
-      return sendEventToKafka(okapiHeaders.get(TENANT), Json.encode(created), SOURCE_RECORD_CREATED, kafkaHeaders,
-        kafkaConfig, key);
+      try {
+        var kafkaHeaders = getKafkaHeaders(okapiHeaders, aRecord.getRecordType());
+        var key = aRecord.getId();
+        return sendEventToKafka(okapiHeaders.get(TENANT), getEvent(aRecord, eventType),
+          eventType.value(), kafkaHeaders, kafkaConfig, key);
+      } catch (Exception e) {
+        LOG.error("Exception during Record domain event sending", e);
+        return Future.failedFuture(e);
+      }
     });
   }
 
@@ -43,6 +62,14 @@ public class RecordDomainEventPublisher {
       KafkaHeader.header(TOKEN, okapiHeaders.get(TOKEN)),
       KafkaHeader.header(RECORD_TYPE, recordType.value()))
     );
+  }
+
+  private String getEvent(Record eventRecord, EventType type) {
+    var event = new SourceRecordDomainEvent()
+      .withId(eventRecord.getId())
+      .withEventType(type)
+      .withEventPayload((String) eventRecord.getParsedRecord().getContent());
+    return Json.encode(event);
   }
 
 }
--- mod-source-record-storage-server/src/main/java/org/folio/services/util/EventHandlingUtil.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/util/EventHandlingUtil.java
@@ -1,12 +1,12 @@
 package org.folio.services.util;
 
+import static java.util.Arrays.stream;
 import static java.util.Objects.nonNull;
 import static org.folio.okapi.common.XOkapiHeaders.TENANT;
 import static org.folio.okapi.common.XOkapiHeaders.TOKEN;
 import static org.folio.okapi.common.XOkapiHeaders.URL;
+import static org.folio.rest.jaxrs.model.SourceRecordDomainEvent.EventType;
 import static org.folio.services.domainevent.RecordDomainEventPublisher.RECORD_DOMAIN_TOPIC;
-import static org.folio.services.domainevent.RecordDomainEventPublisher.SOURCE_RECORD_CREATED;
-import static org.folio.services.domainevent.RecordDomainEventPublisher.SOURCE_RECORD_UPDATED;
 import static org.folio.services.util.KafkaUtil.extractHeaderValue;
 
 import io.vertx.core.Future;
@@ -107,7 +107,7 @@ public final class EventHandlingUtil {
   }
 
   public static String createTopicName(String eventType, String tenantId, KafkaConfig kafkaConfig) {
-    if (SOURCE_RECORD_CREATED.equals(eventType) || SOURCE_RECORD_UPDATED.equals(eventType)) {
+    if (stream(EventType.values()).anyMatch(et -> et.value().equals(eventType))) {
       return KafkaTopicNameHelper.formatTopicName(kafkaConfig.getEnvId(), tenantId, RECORD_DOMAIN_TOPIC);
     }
     return KafkaTopicNameHelper.formatTopicName(kafkaConfig.getEnvId(), KafkaTopicNameHelper.getDefaultNameSpace(),

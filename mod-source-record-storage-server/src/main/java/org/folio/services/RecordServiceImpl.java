88a2470d78d9c4f769f8010bf9f6b1c640baeded
Dmytro Krutii <dmytro_krutii@epam.com>
2024-08-08 14:20:14 +0300

    MODSOURMAN-1203 Add additional validation before marc bib record saving

--- mod-source-record-storage-server/src/main/java/org/folio/services/RecordServiceImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/RecordServiceImpl.java
@@ -15,9 +15,7 @@ import static org.folio.dao.util.RecordDaoUtil.getExternalIdsConditionWithQualif
 import static org.folio.dao.util.SnapshotDaoUtil.SNAPSHOT_NOT_FOUND_TEMPLATE;
 import static org.folio.dao.util.SnapshotDaoUtil.SNAPSHOT_NOT_STARTED_MESSAGE_TEMPLATE;
 import static org.folio.rest.util.QueryParamUtil.toRecordType;
-import static org.folio.services.util.AdditionalFieldsUtil.TAG_999;
-import static org.folio.services.util.AdditionalFieldsUtil.addFieldToMarcRecord;
-import static org.folio.services.util.AdditionalFieldsUtil.getFieldFromMarcRecord;
+import static org.folio.services.util.AdditionalFieldsUtil.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -97,6 +95,7 @@ public class RecordServiceImpl implements RecordService {
   public static final String UPDATE_RECORD_DUPLICATE_EXCEPTION = "Incoming record could be a duplicate, incoming record generation should not be the same as matched record generation and the execution of job should be started after of creating the previous record generation";
   public static final char SUBFIELD_S = 's';
   public static final char INDICATOR = 'f';
+  private static final String TAG_001 = "001";
 
   private final RecordDao recordDao;
 
@@ -126,6 +125,10 @@ public class RecordServiceImpl implements RecordService {
     LOG.debug("saveRecord:: Saving record with id: {} for tenant: {}", record.getId(), tenantId);
     ensureRecordHasId(record);
     ensureRecordHasSuppressDiscovery(record);
+    if (isRecordContainsRequiredField(record)) {
+      LOG.error("saveRecord:: record with id: {} does not contain required fields", record.getId());
+      return Future.failedFuture(new BadRequestException("record does not contain required fields"));
+    }
     return recordDao.executeInTransaction(txQE -> SnapshotDaoUtil.findById(txQE, record.getSnapshotId())
         .map(optionalSnapshot -> optionalSnapshot
           .orElseThrow(() -> new NotFoundException(format(SNAPSHOT_NOT_FOUND_TEMPLATE, record.getSnapshotId()))))
@@ -512,6 +515,19 @@ public class RecordServiceImpl implements RecordService {
       var content = reorderMarcRecordFields(sourceContent, targetContent);
       targetRecord.getParsedRecord().setContent(content);
     }
+  }
+
+  private boolean isRecordContainsRequiredField(Record record) {
+    if (record.getRecordType() == Record.RecordType.MARC_BIB) {
+      var idsHolder = record.getExternalIdsHolder();
+      if (Objects.isNull(idsHolder) || StringUtils.isEmpty(getValueFromControlledField(record, TAG_001))) {
+        return false;
+      }
+      if (StringUtils.isEmpty(idsHolder.getInstanceId()) || StringUtils.isEmpty(idsHolder.getInstanceHrid())) {
+        return false;
+      }
+    }
+    return true;
   }
 
 }

4937542c7f83a9c909e9dcff186b1f4af8d7cd63
Aliaksandr-Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2024-08-15 00:20:41 +0500

    MODSOURCE-787: code review

--- mod-source-record-storage-server/src/main/java/org/folio/dao/RecordDaoImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/dao/RecordDaoImpl.java
@@ -413,7 +413,8 @@ public class RecordDaoImpl implements RecordDao {
     }
 
     return switch (comparisonPartType) {
-      case ALPHANUMERICS_ONLY -> "regexp_replace(\"{partition}\".\"value\", '[^[:alnum:]]', '', 'g')";
+      //case ALPHANUMERICS_ONLY -> "regexp_replace(\"{partition}\".\"value\", '[^[:alnum:]]', '', 'g')";
+      case ALPHANUMERICS_ONLY -> "regexp_replace(\"{partition}\".\"value\", '[^\\w]|_', '', 'g')";
       case NUMERICS_ONLY -> "regexp_replace(\"{partition}\".\"value\", '[^[:digit:]]', '', 'g')";
       default -> DEFAULT_VALUE;
     };

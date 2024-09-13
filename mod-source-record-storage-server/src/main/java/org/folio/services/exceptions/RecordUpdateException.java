865c6415af701dfc1f9c0b0bf14dcb2eb653bbdc
Kateryna_Senchenko <kateryna_senchenko@epam.com>
2024-09-13 17:27:33 +0300

    Added doc for new Exception class

--- mod-source-record-storage-server/src/main/java/org/folio/services/exceptions/RecordUpdateException.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/exceptions/RecordUpdateException.java
@@ -1,8 +1,11 @@
 package org.folio.services.exceptions;
 
+/**
+ * Exception to indicate a record update error
+ */
 public class RecordUpdateException extends RuntimeException {
 
-    public RecordUpdateException(String message) {
-        super(message);
-    }
+  public RecordUpdateException(String message) {
+    super(message);
+  }
 }

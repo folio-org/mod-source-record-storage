86245d325e5a24b6f5e9e651e16bfc735c6d88e8
Javokhir Abdullaev <javokhir_abdullaev@epam.com>
2024-10-16 18:50:20 +0500

    fix tests

--- mod-source-record-storage-server/src/main/java/org/folio/rest/persist/PgConnectOptionsHelper.java
+++ mod-source-record-storage-server/src/main/java/org/folio/rest/persist/PgConnectOptionsHelper.java
@@ -4,11 +4,14 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.pgclient.PgConnectOptions;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.folio.dao.PostgresClientFactory;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PgConnectOptionsHelper {
 
   public static PgConnectOptions createPgConnectOptions(JsonObject sqlConfig) {
-    return PostgresClientInitializer.createPgConnectOptions(sqlConfig, PostgresClient.HOST, PostgresClient.PORT);
+    return PostgresClientInitializer.createPgConnectOptions(sqlConfig,
+      PostgresClientFactory.HOST,
+      PostgresClientFactory.PORT);
   }
 }

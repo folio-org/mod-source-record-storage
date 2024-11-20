e426c0ab3c827fa473955d94ac1b9fcbc30d840d
RomanChernetskyi <111740564+RomanChernetskyi@users.noreply.github.com>
2024-11-20 11:17:56 +0200

    [MODSOURCE-820] Implement marc authority submatch (#650)
    (cherry picked from commit 5d2ee90203f05ceb8846a0d21002fe320acbc672)

--- mod-source-record-storage-server/src/main/java/org/folio/services/handlers/match/AbstractMarcMatchEventHandler.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/handlers/match/AbstractMarcMatchEventHandler.java
@@ -3,6 +3,7 @@ package org.folio.services.handlers.match;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.StringUtils;
@@ -23,6 +24,7 @@ import org.folio.rest.jaxrs.model.Field;
 import org.folio.rest.jaxrs.model.Filter;
 import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
 import org.folio.rest.jaxrs.model.Qualifier;
+import org.folio.rest.jaxrs.model.ReactToType;
 import org.folio.rest.jaxrs.model.Record;
 import org.folio.rest.jaxrs.model.RecordCollection;
 import org.folio.services.caches.ConsortiumConfigurationCache;
@@ -31,6 +33,7 @@ import org.folio.services.util.TypeConnection;
 import org.jooq.Condition;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,7 +42,9 @@ import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
 import static java.lang.String.format;
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.folio.dao.util.RecordDaoUtil.filterRecordByMultipleIds;
 import static org.folio.dao.util.RecordDaoUtil.filterRecordByExternalHrid;
 import static org.folio.dao.util.RecordDaoUtil.filterRecordByExternalId;
 import static org.folio.dao.util.RecordDaoUtil.filterRecordByRecordId;
@@ -53,11 +58,13 @@ import static org.folio.rest.jaxrs.model.ProfileType.MATCH_PROFILE;
 public abstract class AbstractMarcMatchEventHandler implements EventHandler {
   private static final Logger LOG = LogManager.getLogger();
   private static final String PAYLOAD_HAS_NO_DATA_MSG = "Failed to handle event payload, cause event payload context does not contain MARC_BIBLIOGRAPHIC data";
-  private static final String FOUND_MULTIPLE_RECORDS_ERROR_MESSAGE = "Found multiple records matching specified conditions";
+  public static final String FOUND_MULTIPLE_RECORDS_ERROR_MESSAGE = "Found multiple records matching specified conditions";
   private static final String CANNOT_FIND_RECORDS_ERROR_MESSAGE = "Can`t find records matching specified conditions";
   private static final String MATCH_DETAIL_IS_NOT_VALID = "Match detail is not valid: %s";
   private static final String USER_ID_HEADER = "userId";
   public static final String CENTRAL_TENANT_ID = "CENTRAL_TENANT_ID";
+  public static final String MULTI_MATCH_IDS = "MULTI_MATCH_IDS";
+  public static final String ID = "id";
 
   private final TypeConnection typeConnection;
   private final RecordDao recordDao;
@@ -94,7 +101,7 @@ public abstract class AbstractMarcMatchEventHandler implements EventHandler {
       if (isValidMatchDetail(matchDetail)) {
         MatchField matchField = prepareMatchField(record, matchDetail);
         Filter.ComparisonPartType comparisonPartType = prepareComparisonPartType(matchDetail);
-        return retrieveMarcRecords(matchField, comparisonPartType, payload.getTenant())
+        return retrieveMarcRecords(matchField, comparisonPartType, payload, payload.getTenant())
           .compose(localMatchedRecords -> {
             if (isConsortiumAvailable()) {
               return matchCentralTenantIfNeededAndCombineWithLocalMatchedRecords(payload, matchField, comparisonPartType, localMatchedRecords);
@@ -122,7 +129,7 @@ public abstract class AbstractMarcMatchEventHandler implements EventHandler {
         if (consortiumConfigurationOptional.isPresent() && !consortiumConfigurationOptional.get().getCentralTenantId().equals(payload.getTenant())) {
           LOG.debug("matchCentralTenantIfNeededAndCombineWithLocalMatchedRecords:: Matching on centralTenant with id: {}",
             consortiumConfigurationOptional.get().getCentralTenantId());
-          return retrieveMarcRecords(matchField, comparisonPartType, consortiumConfigurationOptional.get().getCentralTenantId())
+          return retrieveMarcRecords(matchField, comparisonPartType, payload, consortiumConfigurationOptional.get().getCentralTenantId())
             .onSuccess(result -> {
               if (!result.isEmpty()) {
                 payload.getContext().put(CENTRAL_TENANT_ID, consortiumConfigurationOptional.get().getCentralTenantId());
@@ -138,14 +145,16 @@ public abstract class AbstractMarcMatchEventHandler implements EventHandler {
     return throwable instanceof MatchingException ? throwable : new MatchingException(throwable);
   }
 
-  private Future<List<Record>> retrieveMarcRecords(MatchField matchField, Filter.ComparisonPartType comparisonPartType, String tenant) {
+  private Future<List<Record>> retrieveMarcRecords(MatchField matchField, Filter.ComparisonPartType comparisonPartType,
+                                                   DataImportEventPayload payload, String tenant) {
+    List<String> matchedRecordIds = getMatchedRecordIds(payload);
     if (matchField.isDefaultField()) {
       LOG.debug("retrieveMarcRecords:: Process default field matching, matchField {}, tenant {}", matchField, tenant);
-      return processDefaultMatchField(matchField, tenant).map(RecordCollection::getRecords);
+      return processDefaultMatchField(matchField, matchedRecordIds, tenant).map(RecordCollection::getRecords);
     }
 
     LOG.debug("retrieveMarcRecords:: Process matched field matching, matchField {}, tenant {}", matchField, tenant);
-    return recordDao.getMatchedRecords(matchField, comparisonPartType, typeConnection, isNonNullExternalIdRequired(), 0, 2, tenant);
+    return recordDao.getMatchedRecords(matchField, comparisonPartType, matchedRecordIds, typeConnection, isNonNullExternalIdRequired(), 0, 2, tenant);
   }
 
   abstract boolean isConsortiumAvailable();
@@ -167,8 +176,10 @@ public abstract class AbstractMarcMatchEventHandler implements EventHandler {
   }
 
   /* Searches for {@link MatchField} in a separate record properties considering it is matched_id, external_id, or external_hrid */
-  private Future<RecordCollection> processDefaultMatchField(MatchField matchField, String tenantId) {
-    Condition condition = filterRecordByState(Record.State.ACTUAL.value());
+  private Future<RecordCollection> processDefaultMatchField(MatchField matchField, List<String> matchedRecordIds, String tenantId) {
+    Condition condition = filterRecordByMultipleIds(matchedRecordIds);
+
+    condition.and(filterRecordByState(Record.State.ACTUAL.value()));
     String valueAsString = getStringValue(matchField.getValue());
     if (matchField.isMatchedId()) {
       condition = condition.and(filterRecordByRecordId(valueAsString));
@@ -245,17 +256,50 @@ public abstract class AbstractMarcMatchEventHandler implements EventHandler {
       return Future.succeededFuture(payload);
     }
     if (records.size() > 1) {
-      constructError(payload, FOUND_MULTIPLE_RECORDS_ERROR_MESSAGE);
-      LOG.warn("processSucceededResult:: Matched multiple record for tenant with id {}", payload.getTenant());
-      return Future.failedFuture(new MatchingException(FOUND_MULTIPLE_RECORDS_ERROR_MESSAGE));
+      if (canProcessMultiMatchResult(payload)) {
+        String multipleRecordIdsList = mapEntityListToIdsJsonString(records);
+        LOG.debug("processSucceededResult:: Found multiple records, proceed matching. Records: {}", multipleRecordIdsList);
+        payload.setEventType(matchedEventType.toString());
+        payload.getContext().put(MULTI_MATCH_IDS, multipleRecordIdsList);
+        return Future.succeededFuture(payload);
+      } else {
+        constructError(payload, FOUND_MULTIPLE_RECORDS_ERROR_MESSAGE);
+        LOG.warn("processSucceededResult:: Matched multiple record for tenant with id {}", payload.getTenant());
+        return Future.failedFuture(new MatchingException(FOUND_MULTIPLE_RECORDS_ERROR_MESSAGE));
+      }
     }
     constructError(payload, CANNOT_FIND_RECORDS_ERROR_MESSAGE);
     return Future.succeededFuture(payload);
+  }
+
+  private boolean canProcessMultiMatchResult(DataImportEventPayload eventPayload) {
+    List<ProfileSnapshotWrapper> childProfiles = eventPayload.getCurrentNode().getChildSnapshotWrappers();
+    return isNotEmpty(childProfiles) && ReactToType.MATCH.equals(childProfiles.get(0).getReactTo())
+      && MATCH_PROFILE.equals(childProfiles.get(0).getContentType());
   }
 
   /* Logic for processing errors */
   private void constructError(DataImportEventPayload payload, String errorMessage) {
     LOG.warn(errorMessage);
     payload.setEventType(notMatchedEventType.toString());
+  }
+
+  private String mapEntityListToIdsJsonString(List<Record> records) {
+    List<String> idList = records.stream()
+      .map(Record::getId)
+      .toList();
+
+    return Json.encode(idList);
+  }
+
+  private List<String> getMatchedRecordIds(DataImportEventPayload payload) {
+    if (StringUtils.isNotEmpty(payload.getContext().get(MULTI_MATCH_IDS))) {
+      return new JsonArray(payload.getContext().remove(MULTI_MATCH_IDS)).stream().map(o -> (String) o).toList();
+    }
+    if (StringUtils.isNotEmpty(payload.getContext().get(getMatchedMarcKey()))) {
+      return List.of(new JsonObject(payload.getContext().get(getMatchedMarcKey())).getString(ID));
+    }
+
+    return Collections.emptyList();
   }
 }

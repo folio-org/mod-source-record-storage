e67eda564e7e28b027d699e3f4545af7e2f4ebd9
Javokhir_Abdullaev <javokhir_abdullaev@epam.com>
2025-01-06 14:35:25 +0500

    add log to investigate script

--- mod-source-record-storage-server/src/main/java/org/folio/dao/RecordDaoImpl.java
+++ mod-source-record-storage-server/src/main/java/org/folio/dao/RecordDaoImpl.java
@@ -827,8 +827,7 @@ public class RecordDaoImpl implements RecordDao {
     } else {
       countQuery = select(inline(null, Integer.class).as(COUNT));
     }
-
-    return dsl
+    var query = dsl
       .with(cte.as(countQuery))
       .select(getAllRecordFieldsWithCount(prt))
       .from(RECORDS_LB)
@@ -840,6 +839,8 @@ public class RecordDaoImpl implements RecordDao {
       .orderBy(orderFields)
       .offset(offset)
       .limit(limit > 0 ? limit : DEFAULT_LIMIT_FOR_GET_RECORDS);
+    LOG.info("query: {}", query);
+    return query;
   }
 
   private RecordsBatchResponse saveRecords(RecordCollection recordCollection, String snapshotId, RecordType recordType,

c2b8e2e2809ec3705dd2d733999a7f01d5613e25
RomanChernetskyi <111740564+RomanChernetskyi@users.noreply.github.com>
2025-02-12 16:17:16 +0200

    [MODSOURCE-854] Use deleteRecordBy id for soft delete of authority (#668)

--- mod-source-record-storage-server/src/main/java/org/folio/consumers/AuthorityDomainKafkaHandler.java
+++ mod-source-record-storage-server/src/main/java/org/folio/consumers/AuthorityDomainKafkaHandler.java
@@ -11,9 +11,9 @@ import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.folio.dao.util.IdType;
 import org.folio.dao.util.RecordType;
 import org.folio.kafka.AsyncRecordHandler;
-import org.folio.rest.jooq.enums.RecordState;
 import org.folio.services.RecordService;
 import org.folio.services.util.KafkaUtil;
 import org.springframework.stereotype.Component;
@@ -54,12 +54,12 @@ public class AuthorityDomainKafkaHandler implements AsyncRecordHandler<String, S
 
     logInput(authorityId, eventSubType, tenantId);
     return (switch (eventSubType) {
-      case SOFT_DELETE -> performSoftDelete(authorityId, tenantId);
+      case SOFT_DELETE -> performSoftDelete(authorityId, tenantId, okapiHeaders);
       case HARD_DELETE -> performHardDelete(authorityId, okapiHeaders);
     }).onFailure(throwable -> logError(authorityId, eventSubType, tenantId));
   }
 
-  private Future<String> performSoftDelete(String authorityId, String tenantId) {
+  private Future<String> performSoftDelete(String authorityId, String tenantId, Map<String, String> okapiHeaders) {
     var condition = filterRecordByExternalId(authorityId);
     return recordService.getRecords(condition, RecordType.MARC_AUTHORITY, Collections.emptyList(), 0, 1, tenantId)
       .compose(recordCollection -> {
@@ -69,7 +69,7 @@ public class AuthorityDomainKafkaHandler implements AsyncRecordHandler<String, S
         }
         var matchedId = recordCollection.getRecords().get(0).getMatchedId();
 
-        return recordService.updateRecordsState(matchedId, RecordState.DELETED, RecordType.MARC_AUTHORITY, tenantId);
+        return recordService.deleteRecordById(matchedId, IdType.RECORD, okapiHeaders);
       }).map(authorityId);
   }
 

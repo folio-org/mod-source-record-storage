1d0e3148467dd3da5f9eb32d85d4e56242e53d2c
Aliaksandr-Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2025-02-17 11:34:49 +0500

    MODSOURMAN-1278: fix error handling

--- mod-source-record-storage-server/src/main/java/org/folio/services/caches/JobProfileSnapshotCache.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/caches/JobProfileSnapshotCache.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -54,19 +55,23 @@ public class JobProfileSnapshotCache {
       .toCompletionStage()
       .toCompletableFuture()
       .thenCompose(httpResponse -> {
-        if (httpResponse.getResponse().statusCode() == HttpStatus.SC_OK) {
-          LOGGER.info("loadJobProfileSnapshot:: JobProfileSnapshot was loaded by id '{}'", profileSnapshotId);
-          return CompletableFuture.completedFuture(Optional.of(Json.decodeValue(httpResponse.getJson().encode(), ProfileSnapshotWrapper.class)));
-        } else if (httpResponse.getResponse().statusCode() == HttpStatus.SC_NOT_FOUND) {
-          LOGGER.warn("loadJobProfileSnapshot:: JobProfileSnapshot was not found by id '{}'", profileSnapshotId);
-          return CompletableFuture.completedFuture(Optional.empty());
-        } else {
-          String message = String.format("loadJobProfileSnapshot:: Error loading jobProfileSnapshot by id: '%s', status code: %s, response message: %s",
-            profileSnapshotId, httpResponse.getResponse().statusCode(), httpResponse.getBody());
-          LOGGER.warn(message);
-          return CompletableFuture.failedFuture(new CacheLoadingException(message));
+        try {
+          if (httpResponse.getResponse().statusCode() == HttpStatus.SC_OK) {
+            LOGGER.info("loadJobProfileSnapshot:: JobProfileSnapshot was loaded by id '{}'", profileSnapshotId);
+            return CompletableFuture.completedFuture(Optional.of(Json.decodeValue(httpResponse.getJson().encode(), ProfileSnapshotWrapper.class)));
+          } else if (httpResponse.getResponse().statusCode() == HttpStatus.SC_NOT_FOUND) {
+            LOGGER.warn("loadJobProfileSnapshot:: JobProfileSnapshot was not found by id '{}'", profileSnapshotId);
+            return CompletableFuture.completedFuture(Optional.empty());
+          } else {
+            String message = String.format("loadJobProfileSnapshot:: Error loading jobProfileSnapshot by id: '%s', status code: %s, response message: %s",
+              profileSnapshotId, httpResponse.getResponse().statusCode(), httpResponse.getBody());
+            LOGGER.warn(message);
+            return CompletableFuture.failedFuture(new CacheLoadingException(message));
+          }
+        } catch (Exception e) {
+          LOGGER.error("loadJobProfileSnapshot:: Error loading jobProfileSnapshot by id: '{}'", profileSnapshotId, e);
+          return CompletableFuture.failedFuture(e);
         }
       });
   }
-
 }

4b34ad4fab2509be68f9c594531ff4d8fd353ee5
Aliaksandr Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2025-11-24 20:59:02 +0500

    MODINV-1294: [RRT] MappingParameters snapshot was not found by jobExecutionId (#714)
    * MODINV-1294: [RRT] MappingParameters snapshot was not found by jobExecutionId

--- mod-source-record-storage-server/src/main/java/org/folio/services/exceptions/CacheLoadingException.java
+++ mod-source-record-storage-server/src/main/java/org/folio/services/exceptions/CacheLoadingException.java
@@ -1,8 +1,22 @@
 package org.folio.services.exceptions;
 
+/**
+ * Exception for signaling errors during cache loading.
+ */
 public class CacheLoadingException extends RuntimeException {
 
   public CacheLoadingException(String message) {
     super(message);
+  }
+
+  /**
+   * Constructs a new CacheLoadingException with the specified detail message and cause.
+   *
+   * @param message the detail message.
+   * @param cause   the cause (which is saved for later retrieval by the
+   *                {@link #getCause()} method).
+   */
+  public CacheLoadingException(String message, Throwable cause) {
+    super(message, cause);
   }
 }
